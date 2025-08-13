29173c0c2675b9834560beb57e13bfa0fc188afa
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-08-14 00:47:17 +0600

    add MS53.1 control ga4 goal

--- websites/magicspoon/MS53-1-PRODUCT-Optimize-Comparison-Chart-Design/control/index.js
+++ websites/magicspoon/MS53-1-PRODUCT-Optimize-Comparison-Chart-Design/control/index.js
@@ -1,15 +1,60 @@
-import testInfo from "./info.json" assert { type: "json" };
+(function MS53_1_TEST() {
+    // ========= TEST START =========
 
-(() => {
     const TEST_CONFIG = {
-        client: "Client Name",
-        project: "Project Name",
-        site_url: "https://www.example.com",
-        test_name: "Ticket Name",
-        page_initials: "AB-TEST",
-        test_variation: 1,
-        test_version: 0.0001,
+        client: "Acadia",
+        project: "Magicspoon",
+        site_url: "https://magicspoon.com/",
+        test_name: `MS53.1: [PRODUCT] Optimize Comparison Chart Design - (2) SET UP TEST`,
+        page_initials: "MS53_1",
+        test_variation: 0 /* 0 -> control, 1, 2 */,
+        test_version: 0.00001,
     };
+
+    function fireGA4Event(eventName, eventLabel = "") {
+        console.log(`MS53_1: Firing GA4 Event: ${eventName} - ${eventLabel}`);
+
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: "GA4event",
+            "ga4-event-name": "cro_event",
+            "ga4-event-p1-name": "event_category",
+            "ga4-event-p1-value": eventName,
+            "ga4-event-p2-name": "event_label",
+            "ga4-event-p2-value": eventLabel,
+        });
+    }
+
+    function isElementVisibleInViewport(el) {
+        let top = el.getBoundingClientRect().top;
+        let right = el.getBoundingClientRect().right;
+        let bottom = el.getBoundingClientRect().bottom;
+        let left = el.getBoundingClientRect().left;
+        let innerWidth = window.innerWidth;
+        let innerHeight = window.innerHeight;
+
+        return ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) && ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth));
+    }
+
+    function scrollHandler(e) {
+        const targetNode = document.querySelector("#product-us-vs-them").previousElementSibling;
+        const isElementVisible = isElementVisibleInViewport(targetNode);
+        if (isElementVisible) {
+            fireGA4Event("MS53_1_View", "Ingredients section (area above us vs them)");
+            window.removeEventListener("scroll", scrollHandler);
+        }
+    }
+
+    function handleIngredientsSectionViewGoal() {
+        const targetNode = document.querySelector("#product-us-vs-them").previousElementSibling;
+        const isElementVisible = isElementVisibleInViewport(targetNode);
+
+        if (isElementVisible) {
+            fireGA4Event("MS53_1_View", "Ingredients section (area above us vs them)");
+        } else {
+            window.addEventListener("scroll", scrollHandler);
+        }
+    }
 
     function waitForElement(predicate, callback, timer = 10000, frequency = 100) {
         try {
@@ -23,31 +68,19 @@ import testInfo from "./info.json" assert { type: "json" };
                 }, frequency);
             }
         } catch (error) {
+            console.warn(error);
             return;
         }
     }
 
     function init() {
-        document.body.classList.add(
-            TEST_CONFIG.page_initials,
-            `${TEST_CONFIG.page_initials}--v${TEST_CONFIG.test_variation}`,
-            `${TEST_CONFIG.page_initials}--version${TEST_CONFIG.test_version}`
-        );
-
-        console.table({ ID: testInfo.id, Variation: testInfo.name });
-
-        console.log(
-            `%c${JSON.stringify(TEST_CONFIG, null, 2)}`,
-            "background: black; border: 2px solid green; color: white; display: block; text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3); text-align: left; font-weight: bold; padding: 10px; margin: 10px; font-family: monospace; white-space: pre;"
-        );
+        console.table(TEST_CONFIG);
+        document.body.classList.add(TEST_CONFIG.page_initials, `${TEST_CONFIG.page_initials}--v${TEST_CONFIG.test_variation}`);
+        handleIngredientsSectionViewGoal();
     }
 
     function hasAllTargetElements() {
-        return !!(
-            document.querySelector(
-                `body:not(.${TEST_CONFIG.page_initials}):not(${TEST_CONFIG.page_initials}--v${TEST_CONFIG.test_variation})`
-            ) && document.querySelector(`.target-element`)
-        );
+        return !!(document.querySelector(`html#product-single body:not(.${TEST_CONFIG.page_initials})`) && document.querySelector(`#product-us-vs-them .width.w-l`));
     }
 
     waitForElement(hasAllTargetElements, init);
