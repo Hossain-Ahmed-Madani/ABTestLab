7a7025f1e1e9444376679e338eea62d225d050b9
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-09-11 18:23:43 +0600

    Update: MS94

--- websites/magicspoon/MS94-SITEWIDE-Default-to-Subscription-2-SET-UP-TEST/v1/compiled/index.js
+++ websites/magicspoon/MS94-SITEWIDE-Default-to-Subscription-2-SET-UP-TEST/v1/compiled/index.js
@@ -11,7 +11,7 @@
         test_name: `MS94: [SITEWIDE] Default to Subscription - (2) SET UP TEST`,
         page_initials: "MS94",
         test_variation: 1,
-        test_version: 0.0002,
+        test_version: 0.0003,
     };
 
     function waitForElement(predicate, callback, timer = 20000, frequency = 150) {
@@ -34,53 +34,18 @@
         () =>
             !!(
                 document.querySelector(`body:not(.${TEST_CONFIG.page_initials})`) &&
-                (document.querySelectorAll(".ms24b-row").length >= 1 ||
-                    document.querySelectorAll(".lsg-bundle-wrapper .lsg-bundle-block").length >= 1 ||
-                    document.querySelectorAll(".rtx-subscription-label").length >= 1)
+                document.querySelectorAll(".ms24b-subscribe, .lsg-bundle-interval-el.lsg-bundle-interval-sub-el, .custom-rtx-subscription-label").length >= 1 &&
+                document.readyState === "complete"
             ),
         () => {
             console.log(TEST_CONFIG);
             document.body.classList.add(TEST_CONFIG.page_initials);
 
-            waitForElement(
-                () => document.querySelectorAll(".ms24b-row").length >= 1,
-                () => {
-                    document.querySelectorAll(".ms24b-row").forEach((item) => {
-                        if (item.classList.contains("ms24b-subscribe")) {
-                            item.classList.add("is-active");
-                        } else {
-                            item.classList.remove("is-active");
-                        }
-                    });
-                }
-            );
 
-            waitForElement(
-                () => document.querySelectorAll(".lsg-bundle-wrapper .lsg-bundle-block.lsg-bundle--otp-selected, .lsg-bundle-interval-el.lsg-bundle-interval-sub-el").length >= 1,
-                () => {
-                    document.querySelectorAll(".lsg-bundle-wrapper .lsg-bundle-block.lsg-bundle--otp-selected").forEach((item) => {
-                        item.classList.add("lsg-bundle--sub-selected");
-                        item.classList.remove("lsg-bundle--otp-selected");
-                    });
-
-                    document.querySelector(".lsg-bundle-interval-el.lsg-bundle-interval-sub-el").click();
-                }
-            );
-
-            waitForElement(
-                () => document.querySelectorAll(".rtx-subscription-label").length >= 1,
-                () => {
-                    document.querySelectorAll(".rtx-subscription-label").forEach((item) => {
-                        if (item.classList.contains("custom-rtx-subscription-label")) {
-                            item.classList.add("is-selected");
-                            item.querySelector("input").setAttribute("checked", "");
-                        } else {
-                            item.classList.remove("is-selected");
-                            item.querySelector("input").removeAttribute("checked");
-                        }
-                    });
-                }
-            );
+            document.querySelectorAll(".ms24b-subscribe, .lsg-bundle-interval-el.lsg-bundle-interval-sub-el, .custom-rtx-subscription-label").forEach((item) => {
+                item.click();
+                window.scrollTo(0, 0);
+            });
         }
     );
 })();
