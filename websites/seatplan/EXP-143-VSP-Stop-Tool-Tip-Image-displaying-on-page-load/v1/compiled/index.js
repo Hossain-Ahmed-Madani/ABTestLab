44a9b7152e78e87c0c805eda82f141dd7ffd3ef1
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-09-12 19:36:29 +0600

    Init: ECX-143

---
+++ websites/seatplan/EXP-143-VSP-Stop-Tool-Tip-Image-displaying-on-page-load/v1/compiled/index.js
@@ -0,0 +1,36 @@
+(() => {
+    const TEST_CONFIG = {
+        client: "SeatPlan",
+        project: "SeatPlan",
+        site_url: "https://seatplan.com/",
+        test_name: "ECX-143: VSP - Stop Tool Tip Image displaying on page load ",
+        page_initials: "AB-EXP-143",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    function waitForElement(predicate, callback, timer = 10000, frequency = 100) {
+        if (timer <= 0) {
+            console.warn(`Timeout reached while waiting for condition: ${predicate.toString()}`);
+            return;
+        } else if (predicate && predicate()) {
+            callback();
+        } else {
+            setTimeout(() => {
+                waitForElement(predicate, callback, timer - frequency, frequency);
+            }, frequency);
+        }
+    }
+
+    function init() {
+        console.table(TEST_CONFIG);
+        const { page_initials, test_variation} = TEST_CONFIG;
+        document.body.classList.add(page_initials, `${page_initials}--v${test_variation}`, `${TEST_CONFIG.page_initials}--version${TEST_CONFIG.test_version}`);
+    }
+
+    function hasAllTargetElements() {
+        return !!(document.querySelector(`body:not(.${TEST_CONFIG.page_initials}):not(${TEST_CONFIG.page_initials}--v${TEST_CONFIG.test_variation})`) && true);
+    }
+
+    waitForElement(hasAllTargetElements, init);
+})();
