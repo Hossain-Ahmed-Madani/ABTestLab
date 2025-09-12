5bca9618f8381c75f0bc3a4aa9da25f3a5dee53d
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-09-12 20:50:23 +0600

    Update: test

--- websites/seatplan/ECX-145-TSM-Seat-Rating-4-5-Pop-Up-AND-Filter/v1/index.js
+++ websites/seatplan/ECX-145-TSM-Seat-Rating-4-5-Pop-Up-AND-Filter/v1/index.js
@@ -1,3 +1,18 @@
+/* 
+Test Name: TSM - Seat Rating 4.5+  Pop Up & Filter
+Ticket: https://seatplan.atlassian.net/jira/software/c/projects/ECX/boards/46?selectedIssue=ECX-145
+Test container: https://app.convert.com/accounts/1004737/projects/1004631/experiences/1004165791/summary
+
+
+Control: https://uat.seatplan.com/london/hamilton-tickets/tickets/30-sep-2025/7-30pm/?_conv_eforce=1004165791.1004391814&utm_campaign=sp5
+V1:  https://uat.seatplan.com/london/hamilton-tickets/tickets/30-sep-2025/7-30pm/?_conv_eforce=1004165791.1004391815&utm_campaign=sp5
+
+
+location: if true -> !!(window.appRoute === "ticketing_date_and_time" && window.highRatedSeatsCount > 0)
+function: window.tsmHighRatedSeatsFilterAbTest();
+
+*/
+
 (function () {
     function waitForElement(predicate, callback, timer = 10000, frequency = 150) {
         if (timer <= 0) {
