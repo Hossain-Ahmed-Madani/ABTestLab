f119acb5136dcecf08209d80563670afe5907ae4
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-09-30 20:25:23 +0600

    add changes

--- websites/electropapa/Electropapa-Test019-Volume-Discount/v2/compiled/index.js
+++ websites/electropapa/Electropapa-Test019-Volume-Discount/v2/compiled/index.js
@@ -4,11 +4,12 @@ Test doc: https://docs.google.com/document/d/13OFhHZ9n1KU_rWYOWWDacs2jVkpWrrBV2Y
 
 Test container: https://app.convert.com/accounts/1004828/projects/10047105/experiences/1004170195/summary
 
-ControL: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?_conv_eforce=1004170195.1004401762&utm_campaign=qa5 
+ControL: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?convert_action=convert_vpreview&convert_e=1004170195&convert_v=1004401762&utm_campaign=qa5 
 V1: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?_conv_eforce=1004170195.1004401763&utm_campaign=qa5 
 v2: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?_conv_eforce=1004170195.1004401764&utm_campaign=qa5 
 
 */
+
 
 (() => {
     const TEST_CONFIG = {
