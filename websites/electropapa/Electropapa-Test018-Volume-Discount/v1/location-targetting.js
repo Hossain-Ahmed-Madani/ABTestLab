7ffbbf656158c45b327caa18d3d7eca302610e06
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-10-07 21:56:58 +0600

    Update: location targetting: Test018

--- websites/electropapa/Electropapa-Test018-Volume-Discount/v1/location-targetting.js
+++ websites/electropapa/Electropapa-Test018-Volume-Discount/v1/location-targetting.js
@@ -1,7 +1,4 @@
 (function () {
-    const TEST_KEY = "CONVERT_1004169319_BUCKETED";
-    const IS_BUCKETED = window[TEST_KEY];
-
     async function waitForPromise(predicate) {
         let count = 0;
 
@@ -16,11 +13,10 @@
                 if (typeof predicate === "function" && predicate()) {
                     observer.disconnect();
                     resolve();
-                } else if (count >= 50) {
+                } else if (count >= 100) {
                     observer.disconnect();
-                    // reject(new Error(`Max polling reached while waiting for: ${count} ' : ' ${predicate.toString()}`));
                 }
-            }).observe(document.body, { childList: true, subtree: true });
+            }).observe(q("body"), { childList: true, subtree: false });
         });
     }
 
@@ -33,18 +29,27 @@
     }
 
     try {
+        if (!q("body")) {
+            throw new Error("No body");
+        }
+
+        const TEST_KEY = "CONVERT_1004169319_BUCKETED";
+        const IS_BUCKETED = window[TEST_KEY];
+
         if (IS_BUCKETED) {
             return true;
         } else {
-            return waitForPromise(() => qq("body.is-ctl-product, .offcanvas").length > 0)
+            // Start mutation observer, but return false synchronously
+            waitForPromise(() => qq("body.is-ctl-product, body.is-ctl-checkout, .offcanvas")?.length > 0)
                 .then(() => {
                     window[TEST_KEY] = true;
                     convert_recheck_experiment();
-                })
-                .catch((err) => false);
+                });
+
+            return false;
         }
     } catch (error) {
         convert_recheck_experiment();
         return false;
     }
-})();
+})()
