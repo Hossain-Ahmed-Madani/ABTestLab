7b4d30dbb61a2d06e3b55c540379132668931c62
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-10-08 16:14:38 +0600

    Add Test019

--- websites/electropapa/Electropapa-Test019-Volume-Discount/v1/index.js
+++ websites/electropapa/Electropapa-Test019-Volume-Discount/v1/index.js
@@ -7,7 +7,10 @@ Test container: https://app.convert.com/accounts/1004828/projects/10047105/exper
 V1: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?_conv_eforce=1004170195.1004401763&utm_campaign=qa5
 v2: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?_conv_eforce=1004170195.1004401764&utm_campaign=qa5
 
+without forced: https://electropapa.com/de/e-bike-akku-als-ersatz-fuer-samsung-gd-ssdi-e24b-sdi-2510b-7inr19-65-4-10inr19-65-4-8-8-ah-24v-li-ion-800108614?utm_campaign=qa5
+
 */
+
 
 (() => {
     const TEST_CONFIG = {
