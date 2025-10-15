d3aca1d4dfecacd1e83214171564e168d0a1fab2
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-10-15 21:58:25 +0600

    Update: build file

--- websites/domyown/ROI--PDP-Add-a-Post-ATC-Modal-Iteration-M/v1/build/index.with-css.js
+++ websites/domyown/ROI--PDP-Add-a-Post-ATC-Modal-Iteration-M/v1/build/index.with-css.js
@@ -4,11 +4,40 @@
       // Check if <head> exists
       clearInterval(interval); // Stop checking once found
       var style = document.createElement("style");
-      style.innerHTML = `.AB-PDP-ATC-MODAL #modal-window-added-product .modal-content {
+      style.innerHTML = `.AB-PDP-ATC-MODAL--overflow-hidden {
+  overflow: hidden;
+}
+.AB-PDP-ATC-MODAL input.add-to-cart[disabled] {
+  pointer-events: none;
+  opacity: 0.7;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .ab-cloned-node .rfk_header {
+  display: none;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .rfk-rw ul li .rfk_name {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  line-clamp: 1;
+  overflow: hidden;
+  margin-bottom: 4px;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .modal-content {
   position: absolute;
   right: -100%;
   top: 0;
   min-height: 100%;
+  overflow: hidden;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .modal-content-body {
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-top: 0px;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .rfk_pimage,
+.AB-PDP-ATC-MODAL #modal-window-added-product .rfk_pimage img {
+  height: 103px;
+  max-height: 103px;
 }
 .AB-PDP-ATC-MODAL
   #modal-window-added-product
@@ -51,7 +80,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 10px;
+  gap: 7px;
 }
 .AB-PDP-ATC-MODAL
   #modal-window-added-product
@@ -61,10 +90,11 @@
   content: "";
   width: 18px;
   height: 11px;
-  border-bottom: 3px solid #0a7806;
-  border-left: 3px solid #0a7806;
+  border-bottom: 4px solid #0a7806;
+  border-left: 4px solid #0a7806;
   display: inline-block;
   transform: rotate(-45deg);
+  margin-top: -3px;
 }
 .AB-PDP-ATC-MODAL #modal-window-added-product .modal-content .close-x {
   position: static;
@@ -74,6 +104,19 @@
 .AB-PDP-ATC-MODAL #modal-window-added-product #braintree-loading-container {
   display: none;
 }
+.AB-PDP-ATC-MODAL
+  #modal-window-added-product
+  h3.heading.font-bold.text-xl.md\:text-2xl.mt-4 {
+  margin-top: 6px;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .quick-view-addons {
+  margin-top: 10px;
+  margin-bottom: 0;
+  padding-top: 5px;
+}
+.AB-PDP-ATC-MODAL #modal-window-added-product .list-reset {
+  margin-bottom: 0;
+}
 .AB-PDP-ATC-MODAL #modal-window-added-product a.button-primary[href="https://www.domyown.com/checkout/payment"]
 {
   display: flex;
@@ -81,7 +124,7 @@
   align-items: center;
   gap: 3px;
 }
-.AB-PDP-ATC-MODAL a.close-modal {
+.AB-PDP-ATC-MODAL a.close-modal:not(.no-margin) {
   display: none;
 }
 .AB-PDP-ATC-MODAL .ab-close-modal {
@@ -95,8 +138,71 @@
 }
 .AB-PDP-ATC-MODAL .modal-content-header,
 .AB-PDP-ATC-MODAL .modal-content-body > div {
-  padding-left: 10px;
-  padding-right: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  > .w-full.md\:w-2\/3 {
+  margin-top: 15px;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  > .w-full.md\:w-2\/3
+  .text-lg {
+  line-height: 100%;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  > .w-full.md\:w-2\/3
+  .text-lg
+  + p {
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 100%;
+  margin: 8px 0 10px;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  > .w-full.md\:w-2\/3
+  .text-lg
+  + p
+  span {
+  color: #315caa;
+}
+.AB-PDP-ATC-MODAL .ab-price-content {
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+}
+.AB-PDP-ATC-MODAL .ab-price-content span {
+  margin-bottom: 0;
+}
+.AB-PDP-ATC-MODAL .ab-price-content span.text-red {
+  font-size: 15px;
+  line-height: 100%;
+  font-weight: 600;
+}
+.AB-PDP-ATC-MODAL .ab-price-content span.line-through {
+  font-size: 13px;
+  line-height: 100%;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  .w-1\/3 {
+  margin-right: 20px;
+}
+.AB-PDP-ATC-MODAL
+  .modal-content-body
+  > div.flex.flex-wrap.md\:flex-no-wrap
+  .w-1\/3
+  img {
+  border: 1px solid #ebebeb;
 }
 .AB-PDP-ATC-MODAL #modal-window-added-product {
   position: fixed;
@@ -146,9 +252,9 @@
     Test info:
         Test name: PDP - Add a Post ATC Modal (Iteration) [M]
         Ticket: https://trello.com/c/7rHQ37aa/4201-pdp-add-a-post-atc-modal-iteration-m
-        Test container: 
+        Test container: https://app.vwo.com/#/test/ab/271/edit/variations/?accountId=348406
         Preview: 
-        Forced variation: 
+        Forced variation: https://www.domyown.com/termidor-sc-p-184.html?_vis_preview_data=eyJhIjoiMTc3MWUwM2QyNzcxMmQ4ZmU0YjRmOThlYzEzMjhhOWQiLCJlIjp7IjI3MSI6eyJ2IjoiMiIsImQiOjAsInMiOjAsInRnIjowLCJ0IjowLCJ0ZCI6MCwibCI6MCwiYWxoIjowLCJpcGxlIjowLCJpaG8iOjAsInBhaGkiOm51bGwsInNhYmVyIjpudWxsLCJuZXdRdWVyeUJveCI6bnVsbCwiZGF0YVJlZ2lvbiI6bnVsbCwibWF0Y2hUeXBlIjpudWxsLCJjbiI6InVuZGVmaW5lZCIsInVybCI6Imh0dHBzJTI1M0ElMjUyRiUyNTJGd3d3LmRvbXlvd24uY29tJTI1MkZ0ZXJtaWRvci1zYy1wLTE4NC5odG1sIiwiYXBwIjoiYXBwIiwidHMiOjE3NjA0MzM0MjI3NTB9fX0=
 */
 
 (() => {
@@ -159,7 +265,7 @@
     test_name: "PDP - Add a Post ATC Modal (Iteration) [M]",
     page_initials: "AB-PDP-ATC-MODAL",
     test_variation: 1,
-    test_version: 0.0001,
+    test_version: 0.0002,
   };
 
   const { page_initials, test_variation, test_version } = TEST_CONFIG;
@@ -228,7 +334,6 @@
       });
 
       const data = await response.json();
-      console.log("Add to Cart Response:", data);
 
       return {
         status: data.response,
@@ -243,10 +348,49 @@
     }
   }
 
+  async function miniCart() {
+    try {
+      const response = await fetch("https://www.domyown.com/cart/mini", {
+        headers: {
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": '"Android"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-csrf-token": "RBePmwuHWyWx43OZ7yx8UbDCZsEKhMXPkFwuKm02",
+          "x-newrelic-id": "VwEGVF9SGwEHUVNXBgY=",
+          "x-requested-with": "XMLHttpRequest",
+        },
+        referrer: "https://www.domyown.com/cart",
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      });
+
+      const data = await response.json();
+
+      return {
+        status: data.response,
+        htmlContent: data.content,
+        cartAmount: data.cartAmount,
+      };
+    } catch (error) {
+      console.error("Mini Cart Error:", error);
+      throw error;
+    }
+  }
+
   function handleAddToCartClick() {
     const submitBtn = q("input.add-to-cart");
     submitBtn.addEventListener("click", (e) => {
       e.preventDefault();
+      e.stopPropagation();
 
       submitBtn.setAttribute("disabled", "true");
 
@@ -262,23 +406,51 @@
 
       addToCart(token, productId, productQuantity, referrer)
         .then(async (result) => {
-          console.log("Cart updated successfully:", result);
-
           // Insert HTML and execute all scripts in order
           await insertHTMLContentNoScript(result.htmlContent);
-
-          console.log("Modal HTML inserted");
         })
         .catch((error) => {
           console.error("Failed to add to cart:", error);
         })
-        .finally(() => {
+        .finally(async () => {
           submitBtn.removeAttribute("disabled");
+          await updateLayout();
+          openModal();
+          handleModalClose();
         });
     });
   }
 
-  function updateModalLayout() {
+  function updateMiniCartLayout(cartAmount) {
+    const miniCart = q("#mini-cart");
+
+    let cartAmountEl = q(miniCart, ".cart-amount");
+
+    if (!cartAmountEl) {
+      cartAmountEl = document.createElement("div");
+      cartAmountEl.setAttribute("id", "cart-amount");
+      cartAmountEl.className =
+        "absolute text-xs text-white bg-red-light rounded-full border-2 border-white h-6 w-6 text-center -pin-r-2 -pin-t-2";
+      miniCart.insertAdjacentElement("beforeend", cartAmountEl);
+    }
+
+    cartAmountEl.innerText = cartAmount;
+  }
+
+  function getPriceContent(htmlContent) {
+    const tmpDiv = document.createElement("div");
+    tmpDiv.innerHTML = htmlContent;
+    const productId = q("#products-grid").getAttribute(
+      "data-selected-products-id",
+    );
+    const priceContent = q(
+      tmpDiv,
+      `tr.product-item-${productId} .text-right.border-t.border-grey-light.align-top.font-bold.py-1`,
+    )?.innerHTML;
+    return priceContent;
+  }
+
+  function updateModalLayout(htmlContent) {
     const modal = q("#modal-window-added-product:not(.ab-modal-updated)");
 
     const checkoutContainer = q(
@@ -305,6 +477,10 @@
       q(modal, ".quick-view-addons div[data-rfkid='rfkid_32']:empty") &&
       !q(modal, ".ab-cloned-node")
     ) {
+      q(
+        modal,
+        ".quick-view-addons div[data-rfkid='rfkid_32']:empty",
+      )?.parentNode.classList.add("hidden");
       const clonedNode = q(
         "#page-content .mt-2.md\\:mt-4.pt-3.md\\:border-t.md\\:pb-4 > div",
       ).cloneNode(true);
@@ -315,12 +491,37 @@
       );
     }
 
-    handleModalClose();
+    const quantityElem = q(
+      modal,
+      ".modal-content-body > div.flex.flex-wrap.md\\:flex-no-wrap > .w-full.md\\:w-2\\/3 p:last-of-type",
+    );
+
+    if (quantityElem && !q(modal, ".ab-price-content")) {
+      const priceContentHTML = getPriceContent(htmlContent);
+
+      quantityElem.insertAdjacentHTML(
+        "afterend",
+        `<div class="ab-price-content">${priceContentHTML}</div>`,
+      );
+      quantityElem.classList.add("hidden");
+    }
+
+    // q(modal, ".w-full.md\\:w-2\\/5.border-l.px-4.border-t.pt-2")?.classList.add("pt-4");
 
     modal.classList.add("ab-modal-updated");
   }
 
+  async function updateLayout() {
+    const { htmlContent, cartAmount } = await miniCart();
+
+    updateModalLayout(htmlContent);
+    updateMiniCartLayout(cartAmount);
+
+    return true;
+  }
+
   function openModal() {
+    const body = q("body");
     const modal = q("#modal-window-added-product");
     const modalBg = q(modal, ".modal-background");
     const modalContent = q(modal, ".modal-content");
@@ -329,46 +530,37 @@
     // Trigger reflow to ensure animations work
     void modal.offsetWidth;
 
+    body.classList.add(TEST_CONFIG.page_initials + "--overflow-hidden");
     modalBg.classList.add("is-active");
     modalContent.classList.add("is-active");
   }
 
   function closeModal() {
+    const body = q("body");
     const modal = q("#modal-window-added-product");
     const modalBg = q(modal, ".modal-background");
     const modalContent = q(modal, ".modal-content");
 
+    body.classList.remove(TEST_CONFIG.page_initials + "--overflow-hidden");
     modalBg.classList.remove("is-active");
     modalContent.classList.remove("is-active");
 
     // Wait for animations to complete before hiding modal
     setTimeout(() => {
       modal.classList.remove("is-active");
-      q(modal, ".close-modal").click();
+      q(modal, ".close-modal:not(.not-margin)").click();
     }, 300);
   }
 
   function handleModalClose() {
-    qq(".modal-background, .ab-close-modal").forEach((item) => {
-      item.addEventListener("click", (e) => {
-        e.preventDefault();
-        closeModal();
-      });
-    });
-  }
-
-  function mutationObserverFunction() {
-    return new MutationObserver((mutationsList, observer) => {
-      if (q("#modal-window-added-product:not(.ab-modal-updated)")) {
-        console.log("Modal added to body");
-        updateModalLayout();
-        openModal();
-      }
-    }).observe(q("body"), {
-      attributes: false,
-      childList: true,
-      subtree: false,
-    });
+    qq(".modal-background, .ab-close-modal, .close-modal.no-margin").forEach(
+      (item) => {
+        item.addEventListener("click", (e) => {
+          e.preventDefault();
+          closeModal();
+        });
+      },
+    );
   }
 
   function init() {
@@ -379,7 +571,6 @@
     );
     console.table(TEST_CONFIG);
     handleAddToCartClick();
-    mutationObserverFunction();
   }
 
   function hasAllTargetElements() {
