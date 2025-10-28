0cb362c304ddd977b8a780f32f8b4803c3057784
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-10-28 14:48:54 +0600

    Initialize test

---
+++ websites/wunderwunsch/NETZ--Test019-Wunderwunsch-PDP-search-bar-displays-suggestion/v1/compiled/index.js
@@ -0,0 +1,57 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "Netzprduzenten",
+        project: "wunderwunsch",
+        site_url: "https://www.wunderwunsch.de/",
+        test_name: "Test019 [Wunderwunsch] - PDP - search bar displays suggestions when activated (mobile only)",
+        page_initials: "TEST019-SEARCH-BAR",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return document.querySelector(s);
+    }
+
+    function init() {
+        document.body.classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.table(TEST_CONFIG);
+    }
+
+    function hasAllTargetElements() {
+        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && true);
+    }
+
+    try {
+        await waitForElementAsync(hasAllTargetElements);
+        init();
+    } catch (error) {
+        console.error(error);
+    }
+})();
---
+++ websites/wunderwunsch/NETZ--Test019-Wunderwunsch-PDP-search-bar-displays-suggestion/v1/index.js
@@ -0,0 +1,94 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "Netzprduzenten",
+        project: "wunderwunsch",
+        site_url: "https://www.wunderwunsch.de/",
+        test_name: "Test019 [Wunderwunsch] - PDP - search bar displays suggestions when activated (mobile only)",
+        page_initials: "TEST019-SEARCH-BAR",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    function waitForElement(predicate, callback, timer = 20000, frequency = 150) {
+        if (timer <= 0) {
+            console.warn(`Timeout reached while waiting for condition: ${predicate.toString()}`);
+            return;
+        } else if (predicate && predicate()) {
+            callback();
+        } else {
+            setTimeout(() => waitForElement(predicate, callback, timer - frequency, frequency), frequency);
+        }
+    }
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    async function waitForPromiseOnMutation(predicate, maxCount = 50) {
+        let count = 0;
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            new MutationObserver((mutationList, observer) => {
+                count++;
+
+                if (typeof predicate === "function" && predicate()) {
+                    observer.disconnect();
+                    return resolve(true);
+                } else if (count > maxCount) {
+                    observer.disconnect();
+                    return reject(new Error(`Max polling count ${count} reached while waiting for predicate:\n${predicate.toString()}`));
+                }
+            }).observe(document.body, { childList: true, subtree: true });
+        });
+    }
+
+    function q(s, o) {
+        return o ? s.querySelector(o) : document.querySelector(s);
+    }
+
+    function qq(s, o) {
+        return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
+    function init() {
+        document.body.classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.table(TEST_CONFIG);
+    }
+
+    function hasAllTargetElements() {
+        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && true);
+    }
+
+    try {
+        await waitForElementAsync(hasAllTargetElements);
+        init()
+    } catch (error) {
+        console.error(error);
+    }
+})();
