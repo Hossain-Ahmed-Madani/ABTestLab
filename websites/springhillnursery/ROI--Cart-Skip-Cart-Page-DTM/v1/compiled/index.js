b430a0b5651d0adc30b5f7ec0f6a774280243f3b
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-11-05 19:00:01 +0600

    Update: changes

--- websites/springhillnursery/ROI--Cart-Skip-Cart-Page-DTM/v1/compiled/index.js
+++ websites/springhillnursery/ROI--Cart-Skip-Cart-Page-DTM/v1/compiled/index.js
@@ -3,8 +3,8 @@ Ticket: https://trello.com/c/ElCqUynL/4316-cart-skip-cart-page-dtm?filter=label%
 Figma: https://www.figma.com/design/9JhvESiGmUH7UEXd0rEUDe/Gardens-Alive-?node-id=1904-193&t=q8YvJUuBScgFyZhE-0
 Test container: https://app.convert.com/accounts/100412411/projects/100416781/experiences/1004174735/summary
 Preview:
-v1: 
-v2:
+v1: https://springhillnursery.com/products/centennial-tulip?_conv_eforce=1004174735.1004411974&utm_campaign=qa5
+v2: https://springhillnursery.com/products/centennial-tulip?_conv_eforce=1004174735.1004411975&utm_campaign=qa5
 
 */
 
@@ -12,7 +12,7 @@ v2:
     const TEST_CONFIG = {
         page_initials: "AB-SKIP-CART-PAGE",
         test_variation: 1 /* 1, 2 */,
-        test_version: 0.0001,
+        test_version: 0.0002,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
@@ -45,24 +45,54 @@ v2:
         return document.querySelector(s);
     }
 
-    async function init() {
-        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
 
-        q(".quick-cart a.quick-cart__submit[href='/cart']").insertAdjacentHTML(
-            "afterend",
-            /* HTML */ `
-                <a href="/checkout" class="ab-side-cart-checkout-btn  btn btn--full btn--primary" aria-label="Go To Checkout">Go To Checkout </a>
-                <a href="${"/cart" }" class="ab-cta-btn" aria-label="${"View Cart" }">
-                    ${"View Cart" }
-                </a>
-            `
-        );
+    function createLayout() {
+        if ((q(".ab-side-cart-checkout-btn") && q(".ab-cta-btn")) || !q(".quick-cart a.quick-cart__submit[href='/cart']")) return;
+
+        const ctaBtn = document.createElement("a");
+        ctaBtn.href = "/cart" ;
+        ctaBtn.className = "ab-cta-btn";
+        ctaBtn.setAttribute("aria-label", "View Cart" );
+        ctaBtn.textContent = "View Cart" ;
+
+        const checkoutBtn = document.createElement("a");
+        checkoutBtn.href = "/checkout";
+        checkoutBtn.className = "ab-side-cart-checkout-btn  btn btn--full btn--primary";
+        checkoutBtn.setAttribute("aria-label", "Go To Checkout");
+        checkoutBtn.textContent = "Go To Checkout";
+
+        const targetNode = q(".quick-cart a.quick-cart__submit[href='/cart']");
+        targetNode.insertAdjacentElement("afterend", ctaBtn);
+        targetNode.insertAdjacentElement("afterend", checkoutBtn);
 
         return;
     }
 
+    function init() {
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        createLayout();
+        mutationObserverFunction();
+    }
+
+    function mutationObserverFunction() {
+        const targetNode = q(".quick-cart");
+        const debouncedUpdate = debounce(createLayout, 250);
+        return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
+    }
+
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && q(".quick-cart a.quick-cart__submit[href='/cart']") );
+        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && q(".quick-cart a.quick-cart__submit[href='/cart']"));
     }
 
     try {
