6444e808f2aeb6b2ce90873bb40fb92c0ef022f6
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-11-13 18:25:34 +0600

    add changes

---
+++ websites/seatplan/All City Production and Venue Pages City Nav Change/v1/goal.js
@@ -0,0 +1,15 @@
+/* 
+Select city nav item | needs js to trigger
+document.querySelectorAll(".city-nav__item, .city-nav__item button, .city-nav__city").forEach((item) => {
+    item.addEventListener("click", () => {
+        console.log("=== City Nav Item Clicked ===");
+    });
+});
+
+Select city sub nav item | can be created with basic selector
+.ab-sub-nav__link, .ab-sub-nav__link *
+
+View city homepage or category page
+^https?:\/\/(?:www\.)?seatplan\.com\/(london|new\-york)(?:\/|\/whats-on(?:\/.*)?)$
+
+*/
