19cee7e34fd417ea382fe9e5327c6cd493a7cb02
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-12-15 19:24:28 +0600

    Update: LME87

--- websites/lemmelive/ACADIA--LME87/v1/compiled/index.js
+++ websites/lemmelive/ACADIA--LME87/v1/compiled/index.js
@@ -12,6 +12,19 @@ https://lemmelive.com/products/byob-5-pack
 
 */
 
+/*
+
+Figma: https://www.figma.com/design/swfYfWcam6dy9Pt4myxAOM/LME87--BYOB-Steps?node-id=18-261&t=kDUIweIcAErm0xzr-1
+Test container: https://marketer.monetate.net/control/a-2087c1e5/p/lemmelive.com/experience/2060558#c2587808:what
+
+URL's
+
+https://lemmelive.com/products/byob-3-pack
+https://lemmelive.com/products/byob-4-pack
+https://lemmelive.com/products/byob-5-pack
+
+*/
+
 const TEST_ID = "LME87";
 const VARIANT_ID = "V1"; /* V1, V2, V3 */
 
@@ -34,7 +47,7 @@ logInfo("fired");
         test_name: "LME87: [BYOB] Add Steps to Bundle Builder - (2) SET UP TEST",
         page_initials: "AB-LME87",
         test_variation: 1 /* 1, 2, 3 */,
-        test_version: 0.0001,
+        test_version: 0.0003,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
@@ -69,6 +82,54 @@ logInfo("fired");
 
     function qq(s, o) {
         return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    function updateOnMutation() {
+        const selectedItemCount = document.querySelectorAll(".bundle-builder__items .bundle-builder__item.active").length;
+
+        const bundleSizeEl = document.querySelector(".bundle-builder__form span[data-bundle-builder-total-count]");
+        const bundleSize = Number(bundleSizeEl?.textContent || 0);
+
+        const progressLevelEl = q(".ab-progress-step.ab-progress-step--start");
+        const progressLevelEndEl = q(".ab-progress-step.ab-progress-step--end");
+        const progressEl = q(".ab-progress__progress");
+
+        // Update counts
+        if (progressLevelEl) progressLevelEl.innerText = selectedItemCount;
+        if (progressLevelEndEl) progressLevelEndEl.innerText = bundleSize;
+
+        // Avoid divide-by-zero + update progress bar
+        if (bundleSize > 0 && progressEl) {
+            const percentage = Math.floor((selectedItemCount / bundleSize) * 100);
+            progressEl.style.width = `${percentage}%`;
+
+            if (progressLevelEl) {
+                progressLevelEl.style.left = `calc(${percentage}% - 2%)`;
+            }
+        }
+
+        // Hide/show left progress bubble
+        if (progressLevelEl) {
+            progressLevelEl.style.display = selectedItemCount === bundleSize ? "none" : "flex";
+        }
+    }
+
+    function mutationObserverFunction() {
+        const targetNode = q(".bundle-builder__items");
+        const debouncedUpdate = debounce(updateOnMutation, 250);
+        return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
     }
 
     function getStepHeading(stepNo, label) {
@@ -126,6 +187,10 @@ logInfo("fired");
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
         createLayout();
+        {
+            updateOnMutation();
+            mutationObserverFunction();
+        }
     }
 
     function checkForItems() {
@@ -141,7 +206,6 @@ logInfo("fired");
         await waitForElementAsync(checkForItems);
         init();
     } catch (error) {
-        console.warn(error);
         return false;
     }
 })();
--- websites/lemmelive/ACADIA--LME87/v1/index.js
+++ websites/lemmelive/ACADIA--LME87/v1/index.js
@@ -12,6 +12,19 @@ https://lemmelive.com/products/byob-5-pack
 
 */
 
+/*
+
+Figma: https://www.figma.com/design/swfYfWcam6dy9Pt4myxAOM/LME87--BYOB-Steps?node-id=18-261&t=kDUIweIcAErm0xzr-1
+Test container: https://marketer.monetate.net/control/a-2087c1e5/p/lemmelive.com/experience/2060558#c2587808:what
+
+URL's
+
+https://lemmelive.com/products/byob-3-pack
+https://lemmelive.com/products/byob-4-pack
+https://lemmelive.com/products/byob-5-pack
+
+*/
+
 const TEST_ID = "LME87";
 const VARIANT_ID = "V1"; /* V1, V2, V3 */
 
@@ -34,7 +47,7 @@ logInfo("fired");
         test_name: "LME87: [BYOB] Add Steps to Bundle Builder - (2) SET UP TEST",
         page_initials: "AB-LME87",
         test_variation: 1 /* 1, 2, 3 */,
-        test_version: 0.0001,
+        test_version: 0.0003,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
@@ -157,11 +170,37 @@ logInfo("fired");
     }
 
     function updateOnMutation() {
-        //
+        const selectedItemCount = document.querySelectorAll(".bundle-builder__items .bundle-builder__item.active").length;
+
+        const bundleSizeEl = document.querySelector(".bundle-builder__form span[data-bundle-builder-total-count]");
+        const bundleSize = Number(bundleSizeEl?.textContent || 0);
+
+        const progressLevelEl = q(".ab-progress-step.ab-progress-step--start");
+        const progressLevelEndEl = q(".ab-progress-step.ab-progress-step--end");
+        const progressEl = q(".ab-progress__progress");
+
+        // Update counts
+        if (progressLevelEl) progressLevelEl.innerText = selectedItemCount;
+        if (progressLevelEndEl) progressLevelEndEl.innerText = bundleSize;
+
+        // Avoid divide-by-zero + update progress bar
+        if (bundleSize > 0 && progressEl) {
+            const percentage = Math.floor((selectedItemCount / bundleSize) * 100);
+            progressEl.style.width = `${percentage}%`;
+
+            if (progressLevelEl) {
+                progressLevelEl.style.left = `calc(${percentage}% - 2%)`;
+            }
+        }
+
+        // Hide/show left progress bubble
+        if (progressLevelEl) {
+            progressLevelEl.style.display = selectedItemCount === bundleSize ? "none" : "flex";
+        }
     }
 
     function mutationObserverFunction() {
-        const targetNode = q("#cart-drawer");
+        const targetNode = q(".bundle-builder__items");
         const debouncedUpdate = debounce(updateOnMutation, 250);
         return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
     }
@@ -221,6 +260,10 @@ logInfo("fired");
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
         createLayout();
+        if (test_variation === 1) {
+            updateOnMutation();
+            mutationObserverFunction();
+        }
     }
 
     function checkForItems() {
@@ -236,7 +279,6 @@ logInfo("fired");
         await waitForElementAsync(checkForItems);
         init();
     } catch (error) {
-        console.warn(error);
         return false;
     }
 })();
