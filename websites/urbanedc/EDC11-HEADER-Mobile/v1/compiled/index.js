77e7b67c87056bd49d3563afa883f0c37eb633fd
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-12-22 14:26:41 +0600

    Init: EDC11

---
+++ websites/urbanedc/EDC11-HEADER-Mobile/v1/compiled/index.js
@@ -0,0 +1,72 @@
+const TEST_ID = "EDC11";
+const VARIANT_ID = "V1"; /* V1, V2 */
+
+function logInfo(message) {
+    console.log(
+        `%cAcadia%c${TEST_ID}-${VARIANT_ID}`,
+        "color: white; background: rgb(0, 0, 57); font-weight: 700; padding: 2px 4px; border-radius: 2px;",
+        "margin-left: 8px; color: white; background: rgb(0, 57, 57); font-weight: 700; padding: 2px 4px; border-radius: 2px;",
+        message
+    );
+}
+
+logInfo("fired");
+
+(async () => {
+    const TEST_CONFIG = {
+        client: "Acadia",
+        project: "urbanedc",
+        host: "https://urbanedc.com/",
+        test_name: "EDC11: [HEADER - Mobile] Add Search Into Header with Panel - (2) SET UP TEST",
+        page_initials: "AB-EDC11",
+        test_variation: 1, /* 1, 2 */
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return document.querySelector(s);
+    }
+
+    function init() {
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.table(TEST_CONFIG);
+    }
+
+    function checkForItems() {
+        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && true);
+    }
+
+    try {
+        await waitForElementAsync(checkForItems);
+        init();
+    } catch (error) {
+        console.warn(error);
+        return false;
+    }
+})();
