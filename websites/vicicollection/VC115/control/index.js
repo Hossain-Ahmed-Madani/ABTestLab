07f4dd2601dbb9753c098bbd7b718e38abd8185f
Hossain-Ahmed-Madani <hossain@echologyx.com>
2025-12-30 20:11:50 +0600

    VC115: control

--- websites/vicicollection/VC115/control/index.js
+++ websites/vicicollection/VC115/control/index.js
@@ -1,39 +1,49 @@
+/* 
+Ticket link: https://trello.com/c/oQ2yiWSy/4560-%F0%9F%92%9B%F0%9F%8D%BF-vc115-collection-filter-quantities-2-set-up-test
+URL:https://www.vicicollection.com/?preview_theme_id=137131098174&pb=1 , https://www.vicicollection.com/collections/new-arrivals , https://www.vicicollection.com/collections/the-daily-drop#page=1 
+Figma: https://www.figma.com/design/OqFHShfnjCQzW7JHGAf9vT/VC_---COLLECTION--Filter-Quantities?node-id=2002-9&t=zIfUG0iOTtzAFOjq-0
+Test container: https://marketer.monetate.net/control/a-41b13725/p/vicicollection.com/experience/2073784#
+
+*/
+
+const TEST_ID = "VC115";
+const VARIANT_ID = "Control"; /* Control, V1 */
+
+function logInfo(message) {
+    console.log(
+        `%cAcadia%c${TEST_ID}-${VARIANT_ID}`,
+        "color: white; background: rgb(0, 0, 57); font-weight: 700; padding: 2px 4px; border-radius: 2px;",
+        "margin-left: 8px; color: white; background: rgb(0, 57, 57); font-weight: 700; padding: 2px 4px; border-radius: 2px;",
+        message
+    );
+}
+
+logInfo("fired");
+
 (async () => {
     const TEST_CONFIG = {
-        client: "Client Name",
-        project: "Project Name",
-        site_url: "https://www.example.com",
-        test_name: "Ticket Name",
-        page_initials: "AB-TEST",
-        test_variation: 1,
+        client: "Acadia",
+        project: "vicicollection",
+        host: "https://www.vicicollection.com/",
+        test_name: "VC115: [COLLECTION] Filter Quantities - (2) SET UP TEST",
+        page_initials: "AB-VC115",
+        test_variation: 0, /* 0, 1 */
         test_version: 0.0001,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
 
-    async function fetchAndParseURLApi(url) {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) throw new Error(`HTTP ${response.status}`);
-
-            const html = await response.text();
-            const dom = new DOMParser().parseFromString(html, "text/html");
-            return dom;
-        } catch (error) {
-            // console.error("Fetch and parse failed:", error);
-            return null;
-        }
-    }
-
-    function waitForElement(predicate, callback, timer = 20000, frequency = 150) {
-        if (timer <= 0) {
-            console.warn(`Timeout reached while waiting for condition: ${predicate.toString()}`);
-            return;
-        } else if (predicate && predicate()) {
-            callback();
-        } else {
-            setTimeout(() => waitForElement(predicate, callback, timer - frequency, frequency), frequency);
-        }
+    function fireGA4Event(eventName, eventLabel = "") {
+        console.log("fireGA4Event", eventName, eventName);
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: "GA4event",
+            "ga4-event-name": "cro_event",
+            "ga4-event-p1-name": "event_category",
+            "ga4-event-p1-value": eventName,
+            "ga4-event-p2-name": "event_label",
+            "ga4-event-p2-value": eventLabel,
+        });
     }
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
@@ -60,72 +70,12 @@
         });
     }
 
-    async function waitForPromiseOnMutation(predicate, maxCount = 50) {
-        let count = 0;
-
-        return new Promise((resolve, reject) => {
-            if (typeof predicate === "function" && predicate()) {
-                return resolve(true);
-            }
-
-            new MutationObserver((mutationList, observer) => {
-                count++;
-
-                if (typeof predicate === "function" && predicate()) {
-                    observer.disconnect();
-                    return resolve(true);
-                } else if (count > maxCount) {
-                    observer.disconnect();
-                    return reject(new Error(`Max polling count ${count} reached while waiting for predicate:\n${predicate.toString()}`));
-                }
-            }).observe(document.body, { childList: true, subtree: true });
-        });
-    }
-
     function q(s, o) {
         return o ? s.querySelector(o) : document.querySelector(s);
     }
 
     function qq(s, o) {
         return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
-    }
-
-    function debounce(func, wait) {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
-
-    function getCookie(key) {
-        try {
-            if (!key || typeof key !== "string") {
-                // console.error("Invalid key provided to getCookie");
-                return null;
-            }
-
-            // Encode the key to handle special characters
-            const encodedKey = encodeURIComponent(key);
-            const cookies = `; ${document.cookie}`;
-
-            // Find the cookie value
-            const parts = cookies.split(`; ${encodedKey}=`);
-
-            if (parts.length === 2) {
-                const value = parts.pop().split(";").shift();
-                return value ? decodeURIComponent(value) : null;
-            }
-
-            return null;
-        } catch (error) {
-            // console.error(`Error reading cookie "${key}":`, error);
-            return null;
-        }
     }
 
     function isSafari() {
@@ -137,19 +87,39 @@
         return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
     }
 
-    function mutationObserverFunction() {
-        const targetNode = q("#cart-drawer");
-        const debouncedUpdate = debounce(updateSideCartLayout, 250);
-        return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
+    function clickFunction() {
+        // Users opens a filter dropdown (Desktop Only)
+        if (window.innerWidth >= 991 || !isTouchEnabled()) {
+            qq(".collection-filters-modal__content, .collection-filter-sort__filter-list").forEach((item) =>
+                item.addEventListener("click", (e) => {
+                    if (e.target.closest(".multi-select__button, .vue-accordion__trigger.btn")) {
+                        fireGA4Event("VC115_FilterView");
+                    }
+                })
+            );
+        }
+
+        // Filter Click (All Device)
+        qq(".collection-filter-sort__more-filters-cta, .collection-filter-sort__filter-button").forEach((item) =>
+            item.addEventListener("click", () => fireGA4Event("VC115_FilterView"))
+        );
     }
 
     function init() {
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
+        clickFunction();
     }
 
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) && true);
+        return !!(
+            q(`body:not(.${page_initials}):not(${page_initials}--v${test_variation})`) &&
+            q(".collection-filters-modal__content input[type='checkbox'][data-count]") &&
+            q("input[type='checkbox'][data-count]") &&
+            q(".collection-filter-sort__filter-list") &&
+            q(".collection-filter-sort__more-filters-cta") &&
+            q(".collection-filter-sort__filter-button")
+        );
     }
 
     try {
