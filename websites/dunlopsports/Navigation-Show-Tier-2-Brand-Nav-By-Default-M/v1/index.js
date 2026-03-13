0774b347193afa408d17a75a5ba045f27b21d213
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-03-14 03:02:32 +0600

    Update: AB-NAV-TIER-2-BRAND-M

--- websites/dunlopsports/Navigation-Show-Tier-2-Brand-Nav-By-Default-M/v1/index.js
+++ websites/dunlopsports/Navigation-Show-Tier-2-Brand-Nav-By-Default-M/v1/index.js
@@ -1,4 +1,4 @@
-(async () => {
+(() => {
     const TEST_CONFIG = {
         client: "ROI Revolutions",
         project: "Dunlop Sports",
@@ -45,10 +45,31 @@
             </svg>
         `,
         "dunlop-logo-svg": /* HTML */ `
-            <svg id="dunlop-logo-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1560.1 253.41">
+            <svg id="dunlop-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1560.1 253.41">
                 <title>Dunlop</title>
                 <path
                     d="M221.53,126.66L3.82,0,74.13,57.86c-97.96,3.44-99.69,130.2-.04,137.65L3.82,253.41,221.53,126.66m-205.2-.02c0-35.56,28.83-64.38,64.39-64.38s64.38,28.82,64.38,64.38-28.83,64.37-64.38,64.37S16.33,162.2,16.33,126.64Z"
+                ></path>
+                <path
+                    d="M30.99,140.25l-5.17,11.34H104.63c16.16,.14,26.97-11.87,28.35-26.65,1.27-13.75-11.2-23.38-23.39-23.25H45.81l-5.75,12.48h11.85l-12.02,26.08h-8.9m53.88-26.65h8.99c7.69-.3,5.31,11.15,5.39,10.21-.5,5.96-5.25,16.38-15.32,15.93l-10.97-.07,11.91-26.06Z"
+                ></path>
+                <path
+                    d="M1544.68,62.05h-174.57l-11.95,25.64h27.21l-35.22,77.34h-27.23l-11.8,25.7h118.63l11.79-25.75h-24.52l12.36-26.88h78.58c10.63,0,25.52-2.98,33.59-16.16,12.61-20.57,32.89-59.9,3.12-59.9m-58.82,34.38c-2.03,7.1-3.45,17.75-17.44,17.86h-28.12l12.16-26.63h28.72c7.22-.02,5.52,5.79,4.68,8.77Z"
+                ></path>
+                <polyline
+                    points="912.51 87.61 933.04 87.61 944.8 62.05 867.73 62.05 855.42 87.61 877.36 87.61 854.78 137.09 814.99 62.05 737.76 62.05 725.69 87.73 748.99 87.73 713.51 165.01 690.14 165.01 678.47 190.74 759.33 190.74 771.06 165.13 749 165.13 771.96 113.87 813.32 190.74 865.17 190.74 912.51 87.61"
+                ></polyline>
+                <path
+                    d="M474.59,113.16c8.08-17.01,14.03-51.11-20.05-51.11h-156.41l-11.99,25.68h24.32l-35.41,77.21h-24.34l-11.91,25.79h154.16c57.83,0,73.56-60.56,81.64-77.57m-100.77,51.78h-30.42l35.28-77.1h21.78c17.01-.9,12.2,11.85,8.23,26.31-3.97,14.46-19.84,50.47-34.87,50.79Z"
+                ></path>
+                <path
+                    d="M553.74,192.14c-.98-.05,16.16,.15,35.79-1.2,25.24-1.74,47.99-7.95,59.92-31l33.93-71.96h22.12l11.98-25.92h-76.88l-11.73,25.67h21.93l-26.15,58.18c-8.72,15.73-13.4,18.28-35.08,17.86-21.69-.43-21.05-10.21-16.8-21.27l24.88-54.77h21.27l11.81-25.67h-113.69l-12,25.67h24.15s-28.47,62.01-28.91,63.28c-15.32,44.2,33.24,40.2,53.46,41.13"
+                ></path>
+                <polyline
+                    points="946.45 165.03 922.75 165.03 911.08 190.73 1083.03 190.73 1112.42 126.67 1087.56 126.68 1054.57 165.03 1016.36 165.03 1051.28 87.71 1073.22 87.71 1085 62.05 969.19 62.05 957.29 87.59 981.77 87.59 946.45 165.03"
+                ></polyline>
+                <path
+                    d="M1137.54,189.13c5.95,1.98,47.73,7.44,97.06,4.68,70.94-.44,89.01-64.01,99.29-92.15,6.78-18.52,6.75-37.38-33.93-41.63-38.76-3.21-82.68-1.18-85.79-.94-18.5,1.48-40.85,3.85-61.63,22.26-5.78,5.18-18.4,21.71-33.32,62.28-13.89,37.76,15.58,44.61,18.32,45.51m51.92-49.4c5.65-15.12,19.23-41.88,22.92-46.7,7.37-10.21,17.01-10.78,39.69-9.08,8.63,1.9,19.72,7.32,7.65,32.89-10.59,22.43-17.36,43.46-29.48,47.65-9.24,3.2-18.81,3.71-32.29,2.08-10.55-1.28-15.78-7.31-8.49-26.84Z"
                 ></path>
             </svg>
         `,
@@ -63,7 +84,7 @@
     };
 
     const DATA = {
-        brands: {
+        brand_items: {
             srixon: {
                 title: "Srixon",
                 link: "/srixon",
@@ -493,31 +514,32 @@
     }
 
     function getNavLayout(menu_items = DATA.menu_items, parent_title = "", level = 1) {
-
         console.log("getNavLayout: recursive call", menu_items, parent_title, level);
-        
+
         return /* HTML */ `
             <div class="ab-nav-menu-container ab-nav-menu-container-level-${level}">
                 ${level === 1
-                    ? /* HTML */ ` 
-                    <div class="ab-nav-menu-top" style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid #e0e0e0;">
-                        <div class="ab-nav-top-logo">LOGO</div>
-                        <div class="ab-nav-close-cta">X</div>
+                    ? ` 
+                    <div class="ab-nav-menu-top">
+                        <div class="ab-nav-top-logo">${ASSETS["dunlop-logo-svg"]}</div>
+                        <div class="ab-nav-close-cta">×</div>
                     </div>`
-                    : /* HTML */ ` 
-                    <div class="ab-nav-menu-header" style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid #e0e0e0;">
-                        <div><button class="ab-nav-menu-back-cta">< ${parent_title}</a></div>
-                        <div class="ab-nav-close-cta">X</div>
-                    </div>`
-                }
+                    : ` 
+                    <div class="ab-nav-menu-header">
+                        <button class="ab-nav-menu-back-cta">
+                            <span class="ab-nav-menu-back-cta__icon"><span class="ab-caret-left"></span></span>
+                            <span class="ab-nav-menu-back-cta__text">${parent_title}</span>
+                        </button>
+                        <div class="ab-nav-close-cta">×</div>
+                    </div>`}
 
                 <ul class="ab-nav-menu-list">
                     ${menu_items
                         .map(
-                            ({ title, link , sub_menu_items = []}) => /* HTML  */ `
+                            ({ title, link, sub_menu_items = [] }) => /* HTML  */ `
                             <li class="ab-nav-menu-item">
-                                <a href="${link}" class="ab-nav-menu-item-link">${title}</a>
-                                ${sub_menu_items && sub_menu_items.length> 0 ? getNavLayout(sub_menu_items, title, level + 1) : ""}
+                                <a href="${link}" class="ab-nav-menu-item-link ${sub_menu_items && sub_menu_items.length > 0 ? "ab-nav-menu-item-link--has-sub-menu" : ""}">${title}</a>
+                                ${sub_menu_items && sub_menu_items.length > 0 ? getNavLayout(sub_menu_items, title, ++level) : ""}
                             </li>
                         `,
                         )
@@ -533,15 +555,35 @@
         targetNode.insertAdjacentHTML(
             "afterend",
             /* HTML */ `
-                <div class="ab-nav-container container-fluid">
-                    <div class="ab-nav-menu-container">${getNavLayout()}</div>
+                <div class="ab-nav-container">
+                    <div class="ab-nav-menu-wrapper">${getNavLayout()}</div>
                 </div>
             `,
         );
     }
 
     function clickFunction() {
-        //
+        q(".ab-nav-container").addEventListener("click", (e) => {
+            const closeButton = e.target.closest('.ab-nav-close-cta');
+            if (closeButton) {
+                q(".close-button button.close").click();
+            }
+
+            const subMenuLinkExpandable = e.target.closest(".ab-nav-menu-item-link.ab-nav-menu-item-link--has-sub-menu");
+            if (subMenuLinkExpandable) {
+                e.preventDefault();
+                const siblingDropdownMenu = subMenuLinkExpandable.nextElementSibling;
+                console.log("siblingDropdownMenu", siblingDropdownMenu);
+                siblingDropdownMenu.classList.add("ab-nav-menu-container--show");
+            }
+
+            const backToParentCta = e.target.closest("button.ab-nav-menu-back-cta");
+            if(backToParentCta) {
+                const parentDropdownMenu = backToParentCta.parentElement.parentElement;
+                console.log("parentDropdownMenu", parentDropdownMenu);
+                parentDropdownMenu.classList.remove("ab-nav-menu-container--show");
+            }
+        });
     }
 
     function init() {
