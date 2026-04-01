56fd82263ca6b9799855901dc542c08ddf1487c1
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-01 21:37:47 +0600

    add changes

--- websites/magicmind/MMI20/v1/compiled/index.js
+++ websites/magicmind/MMI20/v1/compiled/index.js
@@ -61,10 +61,11 @@
         console.log("createV2Layout");
 
         const data = qq(".ingredients-v2__columns .ingredients-v2__col").map((col, idx) => ({
-            col_idx: idx + 1,
+            col_idx: idx,
             accordion_items: qq(col, ".accordion__item").map((item) => ({
                 heading: q(item, ".ingredients-v2__block-btn").textContent.trim(),
                 imgUrl: q(item, "img.ingredients-v2__item-image").getAttribute("src"),
+                is_exclusive: !!q(item, ".ingredients-v2__open-modal-btn"),
                 info_title: q(item, ".ingredients-v2__block-title").textContent.trim(),
                 info_sub_title: q(item, ".ingredients-v2__block-subtitle").textContent.trim(),
                 info_content: q(item, ".ingredients-v2__block-text.accordion__content").innerHTML.trim(),
@@ -74,32 +75,43 @@
         console.log("DATA:", data);
 
         q("ingredients-slider-v2  component-accordions").insertAdjacentHTML(
-            "afterend", 
-            /* HTML */ `
-            <div class="ab-accordion-grid-container">${data.map(
-                ({ col_idx, accordion_items }) => /* HTML */ `
-                    <div class="ab-accordion-grid-item ab-accordion-grid-item--idx-${col_idx}">
-                        ${accordion_items.map(({heading, imgUrl, info_title, info_sub_title, info_content}) => /* HTML */ `
-                            <div class="ab-accordion">
-                                <div class="ab-accordion__left">
-                                    <img src="${imgUrl}" alt="${heading}" width="60" height="60">
-                                </div>
-                                <div class="ab-accordion__right">
-                                    <div class="ab-accordion__head">
-                                        <div class="ab-accordion__title">${info_title}</div> 
-                                        <div class="ab-accordion__chevron">${info_title}</div> 
-                                    </div>
-                                    <div class="ab-accordion__info">
-                                        <div class="ab-accordion__info-title">${info_title}</div>
-                                        <div class="ab-accordion__info-sub-title">${info_sub_title}</div>
-                                        <div class="ab-accordion__info-content">${info_content}</div>
-                                    </div>
-                                </div>
+            "afterend",
+            /* HTML */ ` <div class="ab-accordion-grid-container">
+                ${data
+                    .map(
+                        ({ col_idx, accordion_items }) => /* HTML */ `
+                            <div class="ab-accordion-grid-item ab-accordion-grid-item--idx-${col_idx}">
+                                ${accordion_items
+                                    .map(
+                                        ({ heading, imgUrl, is_exclusive, info_title, info_sub_title, info_content }, idx) => /* HTML */ `
+                                            <div class="ab-accordion ${col_idx === 0 && idx === 0 ? "ab-accordion--expanded" : ""}">
+                                                <div class="ab-accordion__left">
+                                                    <img src="${imgUrl}" alt="${heading}" width="60" height="60" />
+                                                </div>
+                                                <div class="ab-accordion__middle">
+                                                    <div class="ab-accordion__head">
+                                                        <div class="ab-accordion__heading">${heading}</div>
+                                                    </div>
+                                                    <div class="ab-accordion__info">
+                                                        <div class="ab-accordion__info-title">${info_title}</div>
+                                                        ${is_exclusive ? `<div class="ab-accordion__info-exclusive">Exclusive</div>` : ""}
+                                                        <div class="ab-accordion__info-sub-title">${info_sub_title}</div>
+                                                        <div class="ab-accordion__info-content">${info_content}</div>
+                                                    </div>
+                                                </div>
+                                                <div class="ab-accordion__right">
+                                                    <div class="ab-accordion__chevron"></div>
+                                                </div>
+                                            </div>
+                                        `,
+                                    )
+                                    .join("")}
                             </div>
-                        `).join("")}
-                    </div>
-                `)
-                .join("")}</div>`);
+                        `,
+                    )
+                    .join("")}
+            </div>`,
+        );
     }
 
     function init() {
