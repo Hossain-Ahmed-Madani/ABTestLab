dcdfd67eb248a667221fa2347f3258db4bd2faf1
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-07 17:07:45 +0600

    Update: GP102

---
+++ websites/guideposts/GP102/v1/build/index.with-css.js
@@ -0,0 +1,194 @@
+(function () {
+  var interval = setInterval(function () {
+    if (document.head) {
+      // Check if <head> exists
+      clearInterval(interval); // Stop checking once found
+      var style = document.createElement("style");
+      style.innerHTML = `.AB-GP102 .ab-magazine-tag {
+  position: absolute;
+  top: 0;
+  right: 10px;
+  z-index: 2;
+  background-color: #041a72;
+  height: 64px;
+  padding: 8px;
+  border-radius: 0 0 8px 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+}
+.AB-GP102 .ab-magazine-tag__icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.AB-GP102 .ab-magazine-tag__icon svg {
+  width: 20px;
+  height: 20px;
+}
+.AB-GP102 .ab-magazine-tag__text {
+  font-family: STIX Two Text;
+  font-weight: 700;
+  font-style: Bold;
+  font-size: 10px;
+  line-height: 120%;
+  letter-spacing: 0.21px;
+  text-align: center;
+  color: #ffffff;
+}
+@media screen and (min-width: 991px) {
+  .AB-GP102 .ab-magazine-tag {
+    right: 20px;
+  }
+}
+`;
+      document.head.appendChild(style);
+      setTimeout(() => {
+        clearInterval(interval); // Clear the interval after 5 seconds
+      }, 5000);
+    }
+  }, 100); // Check every 100ms for <head>
+})();
+(async () => {
+  const TEST_ID = "GP102";
+  const VARIANT_ID = "V1"; /* Control, V1, V2 */
+
+  function logInfo(message) {
+    console.log(
+      `%cAcadia%c${TEST_ID}-${VARIANT_ID}`,
+      "color:white;background:rgb(0,0,57);font-weight:700;padding:2px 4px;border-radius:2px;",
+      "margin-left:8px;color:white;background:rgb(0,57,57);font-weight:700;padding:2px 4px;border-radius:2px;",
+      message,
+    );
+  }
+
+  logInfo("fired");
+
+  const TEST_CONFIG = {
+    client: "Acadia",
+    project: "Guide Posts",
+    site_url: "https://guideposts.org",
+    test_name: "GP102: [PRODUCT] Magazine Gifting - (2) SET UP TEST",
+    page_initials: "AB-GP102",
+    test_variation: 2 /* 0, 1, 2 */,
+    test_version: 0.0001,
+  };
+
+  const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+  const ASSETS = {
+    gift_icon_svg: /* HTML */ `
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.6667 6.66669H3.33333C2.8731 6.66669 2.5 7.03978 2.5 7.50002V9.16669C2.5 9.62692 2.8731 10 3.33333 10H16.6667C17.1269 10 17.5 9.62692 17.5 9.16669V7.50002C17.5 7.03978 17.1269 6.66669 16.6667 6.66669Z"
+          stroke="white"
+          stroke-width="1.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M10 6.66669V17.5"
+          stroke="white"
+          stroke-width="1.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M15.8334 10V15.8333C15.8334 16.2754 15.6578 16.6993 15.3453 17.0118C15.0327 17.3244 14.6088 17.5 14.1667 17.5H5.83341C5.39139 17.5 4.96746 17.3244 4.6549 17.0118C4.34234 16.6993 4.16675 16.2754 4.16675 15.8333V10"
+          stroke="white"
+          stroke-width="1.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M6.25008 6.66666C5.69755 6.66666 5.16764 6.44717 4.77694 6.05647C4.38624 5.66577 4.16675 5.13587 4.16675 4.58333C4.16675 4.0308 4.38624 3.50089 4.77694 3.11019C5.16764 2.71949 5.69755 2.5 6.25008 2.5C7.05399 2.48599 7.84177 2.87605 8.51069 3.6193C9.17962 4.36255 9.69864 5.4245 10.0001 6.66666C10.3015 5.4245 10.8205 4.36255 11.4895 3.6193C12.1584 2.87605 12.9462 2.48599 13.7501 2.5C14.3026 2.5 14.8325 2.71949 15.2232 3.11019C15.6139 3.50089 15.8334 4.0308 15.8334 4.58333C15.8334 5.13587 15.6139 5.66577 15.2232 6.05647C14.8325 6.44717 14.3026 6.66666 13.7501 6.66666"
+          stroke="white"
+          stroke-width="1.2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    `,
+  };
+
+  async function waitForElementAsync(
+    predicate,
+    timeout = 20000,
+    frequency = 150,
+  ) {
+    const startTime = Date.now();
+
+    return new Promise((resolve, reject) => {
+      if (typeof predicate === "function" && predicate()) {
+        return resolve(true);
+      }
+
+      const interval = setInterval(() => {
+        const elapsed = Date.now() - startTime;
+
+        if (elapsed >= timeout) {
+          clearInterval(interval);
+          return reject(
+            new Error(
+              `Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`,
+            ),
+          );
+        }
+
+        if (typeof predicate === "function" && predicate()) {
+          clearInterval(interval);
+          return resolve(true);
+        }
+      }, frequency);
+    });
+  }
+
+  function q(s, o) {
+    return document.querySelector(s);
+  }
+
+  const TXT = ["Makes a <br/> Great Gift", "Popular <br/> Gift"];
+
+  function init() {
+    q("body").classList.add(
+      page_initials,
+      `${page_initials}--v${test_variation}`,
+      `${page_initials}--version:${test_version}`,
+    );
+    console.table(TEST_CONFIG);
+
+    q(".woocommerce-product-gallery .wpgs-image").insertAdjacentHTML(
+      "afterend",
+      /* HTML */ `
+        <div class="ab-magazine-tag">
+          <div class="ab-magazine-tag__icon">${ASSETS.gift_icon_svg}</div>
+          <div class="ab-magazine-tag__text">${TXT[test_variation - 1]}</div>
+        </div>
+      `,
+    );
+  }
+
+  function checkForItems() {
+    return !!(
+      q(
+        `body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`,
+      ) && q(".woocommerce-product-gallery .wpgs-image")
+    );
+  }
+
+  try {
+    await waitForElementAsync(checkForItems);
+    init();
+  } catch (error) {
+    console.warn(error);
+    return false;
+  }
+})();
--- websites/guideposts/GP102/v1/index.js
+++ websites/guideposts/GP102/v1/index.js
@@ -1,3 +1,16 @@
+/* 
+Test container: https://marketer.monetate.net/control/a-880bc965/p/shopguideposts.org/experience/2102196#
+
+Preview excluding all:
+v1: https://marketer.monetate.net/control/preview/12053/N3NPRQWUEMY6RVF1UA7LZ1RGGDUGQ3NB/102-product-magazine-gifting
+v2: https://marketer.monetate.net/control/preview/12053/LEQTX4SV57Y3YK8WMH0V8S3602V848O5/102-product-magazine-gifting
+
+Preview including all:
+v1: https://marketer.monetate.net/control/preview/12053/LXPDSGEB1HN94DB5X3C5IDHQN6BJK46E/102-product-magazine-gifting
+v2: https://marketer.monetate.net/control/preview/12053/UYOLGGWT5KX59R61A5LFVBSW60N9O4CX/102-product-magazine-gifting
+
+*/
+
 (async () => {
     const TEST_ID = "GP102";
     const VARIANT_ID = "V1"; /* Control, V1, V2 */
@@ -26,15 +39,32 @@
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
 
     const ASSETS = {
-        box_svg: /* HTML */ `
-            <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        gift_icon_svg: /* HTML */ `
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
-                    d="M12.295 3.11952L6.795 0.108272C6.66641 0.0372516 6.5219 0 6.375 0C6.2281 0 6.08359 0.0372516 5.955 0.108272L0.455 3.11952C0.317378 3.19482 0.202532 3.30574 0.122484 3.44065C0.0424362 3.57557 0.000131792 3.72952 0 3.8864V9.8639C0.000131792 10.0208 0.0424362 10.1747 0.122484 10.3096C0.202532 10.4446 0.317378 10.5555 0.455 10.6308L5.955 13.642C6.08363 13.7129 6.22812 13.7501 6.375 13.7501C6.52188 13.7501 6.66637 13.7129 6.795 13.642L12.295 10.6308C12.4326 10.5555 12.5475 10.4446 12.6275 10.3096C12.7076 10.1747 12.7499 10.0208 12.75 9.8639V3.8864C12.7499 3.72952 12.7076 3.57557 12.6275 3.44065C12.5475 3.30574 12.4326 3.19482 12.295 3.11952ZM6.3125 0.765772C6.3309 0.755706 6.35153 0.750431 6.3725 0.750431C6.39347 0.750431 6.4141 0.755706 6.4325 0.765772L11.6569 3.62515L9.53563 4.78577L4.25313 1.89452L6.3125 0.765772ZM6 12.8126L0.8125 9.97327C0.793332 9.96221 0.777446 9.94625 0.766465 9.92703C0.755484 9.90782 0.749802 9.88603 0.75 9.8639V4.2914L6 7.1664V12.8126ZM1.09313 3.62515L3.4725 2.32202L8.75438 5.21327L6.375 6.51515L1.09313 3.62515ZM12 9.8639C12.0002 9.88603 11.9945 9.90782 11.9835 9.92703C11.9726 9.94625 11.9567 9.96221 11.9375 9.97327L6.75 12.8126V7.16515L9 5.93327V8.37515C9 8.4746 9.03951 8.56999 9.10983 8.64031C9.18016 8.71064 9.27554 8.75015 9.375 8.75015C9.47446 8.75015 9.56984 8.71064 9.64017 8.64031C9.71049 8.56999 9.75 8.4746 9.75 8.37515V5.52327L12 4.2914V9.8639Z"
-                    fill="black"
+                    d="M16.6667 6.66669H3.33333C2.8731 6.66669 2.5 7.03978 2.5 7.50002V9.16669C2.5 9.62692 2.8731 10 3.33333 10H16.6667C17.1269 10 17.5 9.62692 17.5 9.16669V7.50002C17.5 7.03978 17.1269 6.66669 16.6667 6.66669Z"
+                    stroke="white"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <path d="M10 6.66669V17.5" stroke="white" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+                <path
+                    d="M15.8334 10V15.8333C15.8334 16.2754 15.6578 16.6993 15.3453 17.0118C15.0327 17.3244 14.6088 17.5 14.1667 17.5H5.83341C5.39139 17.5 4.96746 17.3244 4.6549 17.0118C4.34234 16.6993 4.16675 16.2754 4.16675 15.8333V10"
+                    stroke="white"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <path
+                    d="M6.25008 6.66666C5.69755 6.66666 5.16764 6.44717 4.77694 6.05647C4.38624 5.66577 4.16675 5.13587 4.16675 4.58333C4.16675 4.0308 4.38624 3.50089 4.77694 3.11019C5.16764 2.71949 5.69755 2.5 6.25008 2.5C7.05399 2.48599 7.84177 2.87605 8.51069 3.6193C9.17962 4.36255 9.69864 5.4245 10.0001 6.66666C10.3015 5.4245 10.8205 4.36255 11.4895 3.6193C12.1584 2.87605 12.9462 2.48599 13.7501 2.5C14.3026 2.5 14.8325 2.71949 15.2232 3.11019C15.6139 3.50089 15.8334 4.0308 15.8334 4.58333C15.8334 5.13587 15.6139 5.66577 15.2232 6.05647C14.8325 6.44717 14.3026 6.66666 13.7501 6.66666"
+                    stroke="white"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
                 />
             </svg>
         `,
-
     };
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
@@ -69,18 +99,6 @@
         return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
     }
 
-    function debounce(func, wait) {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
-
     function isSafari() {
         const userAgent = navigator.userAgent;
         return /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
@@ -90,30 +108,25 @@
         return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
     }
 
-    function updateLayout() {
-        if (!q("#CartDrawer-Checkout .money.mw-price")) return;
-
-        q(".ab-usp-item__reward-points").textContent = getRewardPoints();
-    }
-
-    function mutationObserverFunction() {
-        const targetNode = q("#CartDrawer-Checkout");
-        const debouncedUpdate = debounce(updateLayout, 100);
-        return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
-    }
+    const TXT = ["Makes a <br/> Great Gift", "Popular <br/> Gift"];
 
     function init() {
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
 
-      //
+        q(".woocommerce-product-gallery .wpgs-image").insertAdjacentHTML(
+            "afterend",
+            /* HTML */ `
+                <div class="ab-magazine-tag">
+                    <div class="ab-magazine-tag__icon">${ASSETS.gift_icon_svg}</div>
+                    <div class="ab-magazine-tag__text">${TXT[test_variation - 1]}</div>
+                </div>
+            `,
+        );
     }
 
     function checkForItems() {
-        return !!(
-            q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) &&
-            true
-        );
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q(".woocommerce-product-gallery .wpgs-image"));
     }
 
     try {
