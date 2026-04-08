14d588b1c49dbc1fbf752db2f3f0735f666235f9
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-08 18:07:50 +0600

    add new changes

--- websites/texastaxprotest/TTP37/v1/compiled/index.js
+++ websites/texastaxprotest/TTP37/v1/compiled/index.js
@@ -11,6 +11,16 @@ https://www.texastaxprotest.com/facebook-landing-page/
 https://www.texastaxprotest.com/blog-contact-us/
 
 
+Test container: https://marketer.monetate.net/control/a-7b7b9c2b/p/texastaxprotest.com/experience/2103664
+
+Preview including all:
+V1: https://marketer.monetate.net/control/preview/12476/3RK4Y5UH3MPV0DDHSZTR5ZJH8BIF4TGR/ttp37-landing-page-counting-up-engagement-section
+V2: https://marketer.monetate.net/control/preview/12476/A8V66KGI07342OTP2LH88METNRKWK8E6/ttp37-landing-page-counting-up-engagement-section
+
+Preview excluding all:
+V1: https://marketer.monetate.net/control/preview/12476/1S0SB22GRQBQSMJQKNJ0LV06D37WPVVF/ttp37-landing-page-counting-up-engagement-section
+V2: https://marketer.monetate.net/control/preview/12476/NEP0ML9L42RI7IJANGN1QY0P41263P76/ttp37-landing-page-counting-up-engagement-section
+
 */
 
 (async () => {
@@ -34,7 +44,7 @@ https://www.texastaxprotest.com/blog-contact-us/
         site_url: "https://www.texastaxprotest.com/",
         test_name: "TTP37: [Landing Page] Counting Up Engagement Section-(2) SET UP TEST",
         page_initials: "AB-TTP37",
-        test_variation: 2,
+        test_variation: 1,
         test_version: 0.0001,
     };
 
@@ -105,7 +115,21 @@ https://www.texastaxprotest.com/blog-contact-us/
     }
 
     function createLayoutV2() {
-        q("body > .mantine-Container-root").insertAdjacentHTML("afterbegin", /* HTML */ ` <div>My New Content V2</div> `);
+        q("body > .mantine-Container-root").insertAdjacentHTML(
+            "afterbegin",
+            /* HTML */ `
+                <div class="ab-engagement-section">
+                    <div class="ab-engagement-counter-wrapper">
+                        <div class="ab-engagement-counter">${counterValue}</div>
+                    </div>
+                    <div class="ab-engagement-divider"></div>
+                    <div class="ab-engagement-subtitle">
+                        Appeals won for Texas <br />
+                        homeowners last year.
+                    </div>
+                </div>
+            `,
+        );
     }
 
     function updateCounterValue() {
@@ -186,7 +210,6 @@ https://www.texastaxprotest.com/blog-contact-us/
             }
 
             updateCounterValue();
-
         } catch (error) {
             console.warn(error);
             return false;
--- websites/texastaxprotest/TTP37/v1/index.js
+++ websites/texastaxprotest/TTP37/v1/index.js
@@ -11,6 +11,16 @@ https://www.texastaxprotest.com/facebook-landing-page/
 https://www.texastaxprotest.com/blog-contact-us/
 
 
+Test container: https://marketer.monetate.net/control/a-7b7b9c2b/p/texastaxprotest.com/experience/2103664
+
+Preview including all:
+V1: https://marketer.monetate.net/control/preview/12476/3RK4Y5UH3MPV0DDHSZTR5ZJH8BIF4TGR/ttp37-landing-page-counting-up-engagement-section
+V2: https://marketer.monetate.net/control/preview/12476/A8V66KGI07342OTP2LH88METNRKWK8E6/ttp37-landing-page-counting-up-engagement-section
+
+Preview excluding all:
+V1: https://marketer.monetate.net/control/preview/12476/1S0SB22GRQBQSMJQKNJ0LV06D37WPVVF/ttp37-landing-page-counting-up-engagement-section
+V2: https://marketer.monetate.net/control/preview/12476/NEP0ML9L42RI7IJANGN1QY0P41263P76/ttp37-landing-page-counting-up-engagement-section
+
 */
 
 (async () => {
@@ -34,7 +44,7 @@ https://www.texastaxprotest.com/blog-contact-us/
         site_url: "https://www.texastaxprotest.com/",
         test_name: "TTP37: [Landing Page] Counting Up Engagement Section-(2) SET UP TEST",
         page_initials: "AB-TTP37",
-        test_variation: 2,
+        test_variation: 1,
         test_version: 0.0001,
     };
 
@@ -44,20 +54,6 @@ https://www.texastaxprotest.com/blog-contact-us/
         1: ["$84,900,995", "$84,900,996", "$84,900,997", "$84,900,998", "$84,900,999", "$85,000,000+"],
         2: ["4,996", "4,997", "4,998", "4,999", "5,000+"],
     };
-
-    function fireGA4Event(eventName, eventLabel = "") {
-        console.log("fireGA4Event", eventName, eventLabel);
-
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-            event: "GA4event",
-            "ga4-event-name": "cro_event",
-            "ga4-event-p1-name": "event_category",
-            "ga4-event-p1-value": eventName,
-            "ga4-event-p2-name": "event_label",
-            "ga4-event-p2-value": eventLabel,
-        });
-    }
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
         const startTime = Date.now();
@@ -132,7 +128,21 @@ https://www.texastaxprotest.com/blog-contact-us/
     }
 
     function createLayoutV2() {
-        q("body > .mantine-Container-root").insertAdjacentHTML("afterbegin", /* HTML */ ` <div>My New Content V2</div> `);
+        q("body > .mantine-Container-root").insertAdjacentHTML(
+            "afterbegin",
+            /* HTML */ `
+                <div class="ab-engagement-section">
+                    <div class="ab-engagement-counter-wrapper">
+                        <div class="ab-engagement-counter">${counterValue}</div>
+                    </div>
+                    <div class="ab-engagement-divider"></div>
+                    <div class="ab-engagement-subtitle">
+                        Appeals won for Texas <br />
+                        homeowners last year.
+                    </div>
+                </div>
+            `,
+        );
     }
 
     function updateCounterValue() {
@@ -213,7 +223,6 @@ https://www.texastaxprotest.com/blog-contact-us/
             }
 
             updateCounterValue();
-
         } catch (error) {
             console.warn(error);
             return false;
