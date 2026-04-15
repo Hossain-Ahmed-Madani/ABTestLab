6fd142889e36f4ea120779691cff423de7b99c70
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-15 20:11:46 +0600

    add mobile design

--- websites/hookandloop/EXP-299-Display-Pricing-Table-On-PDP/v1/index.js
+++ websites/hookandloop/EXP-299-Display-Pricing-Table-On-PDP/v1/index.js
@@ -151,7 +151,58 @@
 
         q(".flex.flex-col.sm\\:flex-row.md\\:flex-col.xl\\:flex-row.items-end.my-4 > div[role='group']").insertAdjacentHTML(
             "beforeend",
-            /* HTML */ `<div class="ab-pricing-table">New Pricing Table</div>`,
+            /* HTML */ `
+                <div class="ab-pricing-table-wrapper">
+                    <div class="ab-pricing-header">
+                        <div class="ab-pricing-header__left">Volume Discount</div>
+                        <div class="ab-pricing-header__right">Auto-applied at checkout</div>
+                    </div>
+                    <div class="ab-pricing-table">
+                        <div class="ab-pricing-table__sub-header">✦ Add 11 more roll to unlock 10% off</div>
+                        <ul class="ab-pricing-table__pricing ab-pricing-table__pricing--mobile">
+                            ${Array.from({ length: 3 })
+                                .map(
+                                    (_, index) => `
+                                    <li class="ab-pricing-table__pricing__item">
+                                
+                                    <div class="ab-pricing-table__pricing__save-percentage">-10%</div>
+                                            <div class="ab-pricing-table__pricing__roll-count">6 ROLLS+</div>
+                                            <div class="ab-pricing-table__pricing__price">$11.25</div>
+                                            <div class="ab-pricing-table__pricing__price-per-yard">($0.45/yard)</div>
+                                            <div class="ab-pricing-table__pricing__save">You save $25.00</div>
+                                    </li>
+                                    `,
+                                )
+                                .join("")}
+                        </ul>
+                        <ul class="ab-pricing-table__pricing ab-pricing-table__pricing--desktop" style="border:1px solid red;">
+                            ${Array.from({ length: 3 })
+                                .map(
+                                    (_, index) => `
+                                    <li class="ab-pricing-table__pricing__item">
+                                        <div class="ab-pricing-table__pricing__left">
+                                            <div class="ab-pricing-table__pricing__roll-count">6 ROLLS+</div>
+                                            <div class="ab-pricing-table__pricing__save">You save $25.00</div>
+                                            <div class="ab-pricing-table__pricing__save-percentage">-10%</div>
+                                        </div>
+                                        <div class="ab-pricing-table__pricing__item__right">
+                                            <div class="ab-pricing-table__pricing__price">$11.25</div>
+                                            <div class="ab-pricing-table__pricing__price-per-yard">($0.45/yard)</div>
+                                        </div>
+                                    </li>
+                                    `,
+                                )
+                                .join("")}
+                        </ul>
+                        <div class="ab-pricing-table__footer">
+                            <div class="ab-pricing-table__footer__left"></div>
+                            <div class="ab-pricing-table__footer__right">
+                                <a href="https://www.hookandloop.com/price-sheet?sku=Fasteners-DG-Sew-On" target="_blank" class="">See Full Price List →</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `,
         );
     }
 
