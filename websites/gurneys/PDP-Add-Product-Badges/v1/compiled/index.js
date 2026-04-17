6b2578f30932c13918b99235da8d10437051646c
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-17 18:07:02 +0600

    Update: PDP - Add Product Badges [DTM]

--- websites/gurneys/PDP-Add-Product-Badges/v1/compiled/index.js
+++ websites/gurneys/PDP-Add-Product-Badges/v1/compiled/index.js
@@ -10,10 +10,49 @@ Forced Variation: https://www.gurneys.com/products/gurney-girls-best-vff-hybrid-
     const TEST_CONFIG = {
         page_initials: "AB-PRODUCT-BADGES",
         test_variation: 1,
-        test_version: 0.0002,
+        test_version: 0.0004,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    const DATA = [
+        {
+            urlList: [
+                "https://www.gurneys.com/products/gurneys_whopper_junebearing_strawberry",
+                "https://www.gurneys.com/products/yukon_gold_potatoes",
+                "https://www.gurneys.com/products/simply-irresistible-sweet-corn",
+                "https://www.gurneys.com/products/candy_hybrid_onion",
+                "https://www.gurneys.com/products/asparabest-asparagus",
+                "https://www.gurneys.com/products/covington-sweet-potato",
+                "https://www.gurneys.com/products/dwarf_burning_bush_",
+                "https://www.gurneys.com/products/contender_peach_tree_",
+                "https://www.gurneys.com/products/dwarf_meyer_lemon",
+                "https://www.gurneys.com/products/heritage_raspberry",
+            ],
+            badge: {
+                label: "Bestseller",
+                className: "ab-product-badge--bestseller",
+            },
+        },
+        {
+            urlList: [
+                "https://www.gurneys.com/products/gotta_have_it_sweet_corn",
+                "https://www.gurneys.com/products/gurney-girls-best-vff-hybrid-tomato",
+                "https://www.gurneys.com/products/gurneys_giant_ii_hybrid_sweet_pepper_",
+                "https://www.gurneys.com/products/kabluey_blueberry_",
+                "https://www.gurneys.com/products/crunch-a-bunch-hybrid-carrot",
+                "https://www.gurneys.com/products/juliet-dwarf-cherry",
+                "https://www.gurneys.com/products/beet-unbeetable-hybrid",
+                "https://www.gurneys.com/products/gurneys_beneficial_bug_blend_",
+                "https://www.gurneys.com/products/razzmatazz",
+                "https://www.gurneys.com/products/gurneys_sunny_hedge_blend_sunflower",
+            ],
+            badge: {
+                label: "Only From Gurney's",
+                className: "ab-product-badge--only-from-gurneys",
+            },
+        },
+    ];
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
         const startTime = Date.now();
@@ -46,6 +85,11 @@ Forced Variation: https://www.gurneys.com/products/gurney-girls-best-vff-hybrid-
     function init() {
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
 
+        const currentUrl = window.location.href;
+        const badgeData = DATA.filter((data) =>  data.urlList.some(url => currentUrl.includes(url)));
+
+        if (!badgeData) return;
+
         q("head").insertAdjacentHTML(
             "beforeend",
             /* HTML */ `
@@ -58,10 +102,15 @@ Forced Variation: https://www.gurneys.com/products/gurney-girls-best-vff-hybrid-
         q(".product__block--product-header-inner").insertAdjacentHTML(
             "afterbegin",
             /* HTML */ `
-                <div class="ab-product-badges-container">
-                    <div class="ab-product-badge ab-product-badge--only-from-gurneys">Only From Gurney's</div>
-                    <div class="ab-product-badge ab-product-badge--bestseller">Bestseller</div>
-                </div>
+                ${badgeData
+                    .map(
+                        ({ badge }) => /* HTML */ `
+                            <div class="ab-product-badges-container">
+                                <div class="ab-product-badge ${badge.className}">${badge.label}</div>
+                            </div>
+                        `,
+                    )
+                    .join("")}
             `,
         );
     }
