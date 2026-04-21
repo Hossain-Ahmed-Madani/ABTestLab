7719b335dc2d499cea934032646edbd8381aba2b
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-21 21:49:49 +0600

    Update: [ECX - 188] Production - Seat View Messaging, dd goal code

--- websites/seatplan/ECX-188/v1/goal.js
+++ websites/seatplan/ECX-188/v1/goal.js
@@ -1,24 +1,10 @@
-/* 
-
-https://app.convert.com/accounts/1004737/projects/1004631/experiences/1004194313/summary ->  Interact with calendar | Production Page | JS
-
-
-Test container: https://app.convert.com/accounts/1004737/projects/1004631/experiences/1004195515/summary
-
-Forced Variation:
-
-control: https://seatplan.com/london/the-jungle-book-tickets/?_conv_eforce=1004195515.1004459737&utm_campaign=qa5
-variation: https://seatplan.com/london/the-jungle-book-tickets/?_conv_eforce=1004195515.1004459738&utm_campaign=qa5
-
-*/
-
 (async () => {
     const TEST_CONFIG = {
         client: "Seatplan",
         project: "Seatplan",
         site_url: "https://seatplan.com",
-        test_name: "[ECX - 188] Production - Seat View Messaging",
-        page_initials: "AB-EXP-188",
+        test_name: "[ECX - 188] Production - Seat View Messaging | GLOBl",
+        page_initials: "GLOBAL-EXP-188",
         test_variation: 1,
         test_version: 0.0001,
     };
@@ -32,7 +18,8 @@ variation: https://seatplan.com/london/the-jungle-book-tickets/?_conv_eforce=100
                     d="M10 14.5C11.25 14.5 12.3125 14.0625 13.1875 13.1875C14.0625 12.3125 14.5 11.25 14.5 10C14.5 8.75 14.0625 7.6875 13.1875 6.8125C12.3125 5.9375 11.25 5.5 10 5.5C8.75 5.5 7.6875 5.9375 6.8125 6.8125C5.9375 7.6875 5.5 8.75 5.5 10C5.5 11.25 5.9375 12.3125 6.8125 13.1875C7.6875 14.0625 8.75 14.5 10 14.5ZM10 12.5C9.3 12.5 8.70833 12.2583 8.225 11.775C7.74167 11.2917 7.5 10.7 7.5 10C7.5 9.3 7.74167 8.70833 8.225 8.225C8.70833 7.74167 9.3 7.5 10 7.5C10.7 7.5 11.2917 7.74167 11.775 8.225C12.2583 8.70833 12.5 9.3 12.5 10C12.5 10.7 12.2583 11.2917 11.775 11.775C11.2917 12.2583 10.7 12.5 10 12.5ZM2 18C1.45 18 0.979167 17.8042 0.5875 17.4125C0.195833 17.0208 0 16.55 0 16V4C0 3.45 0.195833 2.97917 0.5875 2.5875C0.979167 2.19583 1.45 2 2 2H5.15L7 0H13L14.85 2H18C18.55 2 19.0208 2.19583 19.4125 2.5875C19.8042 2.97917 20 3.45 20 4V16C20 16.55 19.8042 17.0208 19.4125 17.4125C19.0208 17.8042 18.55 18 18 18H2Z"
                     fill="#025782"
                 />
-            </svg> `,
+            </svg>
+        `,
     };
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
@@ -71,29 +58,15 @@ variation: https://seatplan.com/london/the-jungle-book-tickets/?_conv_eforce=100
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
 
-        q(".production-overview__nav-section").insertAdjacentHTML(
-            "afterend",
-            /* HTML */ `
-                <section class="ab-seat-view-messaging-section">
-                    <div class="ab-seat-view-messaging-item">
-                        <div class="ab-seat-view-messaging-item__icon">${ASSETS.camera_svg}</div>
-                        <div class="ab-seat-view-messaging-item__text-content">
-                            <strong>Book with confidence.</strong> 
-                            <br/>
-                            <span>See real seat view photos before you buy.</span>
-                        </div>
-                    </div>
-                </section>
-            `,
-        );
+        q(".ticket-calendar").addEventListener("click", (e) => {
+            console.log("GOAL: Interact with calendar | Production Page | JS");
+            window._conv_q = window._conv_q || [];
+            _conv_q.push(["triggerConversion", "1004110604"]);
+        });
     }
 
     function checkForItems() {
-        return !!(
-            q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) &&
-            q(".production-overview__nav-section") &&
-            q(".production-page-section--long-description:not(:has(#seats-views))")
-        );
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q(".ticket-calendar"));
     }
 
     try {
