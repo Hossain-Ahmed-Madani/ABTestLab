2a1871c6208e2a8c7324d95016494ca55caec330
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-04-30 20:06:19 +0600

    Update: initial bucket action and added set time out

--- websites/seatplan/ECX-189/v1/index.js
+++ websites/seatplan/ECX-189/v1/index.js
@@ -7,7 +7,6 @@ Forced Variation: https://seatplan.com/?_conv_eforce=1004196271.1004461480&utm_c
 
 */
 
-
 (async () => {
     const TEST_CONFIG = {
         client: "SeatPlan",
@@ -16,7 +15,7 @@ Forced Variation: https://seatplan.com/?_conv_eforce=1004196271.1004461480&utm_c
         test_name: "[ECX-189] Mobile - Homepage - Smooth scroll hero search field to top of page when selected",
         page_initials: "AB-EXP-189",
         test_variation: 1,
-        test_version: 0.0001,
+        test_version: 0.0003,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
@@ -63,14 +62,14 @@ Forced Variation: https://seatplan.com/?_conv_eforce=1004196271.1004461480&utm_c
     }
 
     function moveToTopIfNeeded(targetNode) {
-        if(!targetNode) return;
-        
+        if (!targetNode) return;
+
         const rect = targetNode.getBoundingClientRect();
         const tolerance = 2;
-        if (Math.abs(rect.top - 100) <= tolerance) {
-            return; 
+        if (Math.abs(rect.top - 10) <= tolerance) {
+            return;
         }
-        const scrollOffset = rect.top - 100;
+        const scrollOffset = rect.top - 10;
         window.scrollBy({
             top: scrollOffset,
             behavior: "smooth",
@@ -80,11 +79,15 @@ Forced Variation: https://seatplan.com/?_conv_eforce=1004196271.1004461480&utm_c
     function init() {
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
+        
+        const targetNode = q("#search-input");
+
+        setTimeout(() => {
+            console.log("targetNode", targetNode);
+            moveToTopIfNeeded(targetNode);
+        }, 150);
 
         const eventName = isTouchEnabled() ? "touchend" : "click";
-
-        const targetNode = q("#search-input");
-        moveToTopIfNeeded(targetNode);
         targetNode.addEventListener(eventName, () => moveToTopIfNeeded(targetNode));
     }
 
