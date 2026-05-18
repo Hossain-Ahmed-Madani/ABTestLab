663e08688ac356ab82cbdea5e0e11b1f11f1f423
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-05-18 18:40:06 +0600

    Update: PMO52

--- websites/water/PMO32/v1/compiled/index.js
+++ websites/water/PMO32/v1/compiled/index.js
@@ -19,11 +19,39 @@
         site_url: "https://www.water.com",
         test_name: "PMO52: [CART] Add Social Proof-(2) SET UP TEST",
         page_initials: "AB-PMO52",
-        test_variation: 1,
+        test_variation: 2, /* 1, 2 */
         test_version: 0.0001,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    const ASSETS = {
+        truck_svg: /* HTML */ `
+            <svg width="26" height="16" viewBox="0 0 26 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M8.34001 14.84C8.91464 14.84 9.46574 14.6118 9.87207 14.2054C10.2784 13.7991 10.5067 13.248 10.5067 12.6734C10.5067 12.0987 10.2784 11.5476 9.87207 11.1413C9.46574 10.735 8.91464 10.5067 8.34001 10.5067C7.76537 10.5067 7.21427 10.735 6.80794 11.1413C6.40161 11.5476 6.17334 12.0987 6.17334 12.6734C6.17334 13.248 6.40161 13.7991 6.80794 14.2054C7.21427 14.6118 7.76537 14.84 8.34001 14.84ZM19.1733 14.84C19.748 14.84 20.2991 14.6118 20.7054 14.2054C21.1117 13.7991 21.34 13.248 21.34 12.6734C21.34 12.0987 21.1117 11.5476 20.7054 11.1413C20.2991 10.735 19.748 10.5067 19.1733 10.5067C18.5987 10.5067 18.0476 10.735 17.6413 11.1413C17.2349 11.5476 17.0067 12.0987 17.0067 12.6734C17.0067 13.248 17.2349 13.7991 17.6413 14.2054C18.0476 14.6118 18.5987 14.84 19.1733 14.84Z"
+                    stroke="#14A7E0"
+                    stroke-width="1.51338"
+                    stroke-miterlimit="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <path
+                    d="M10.5609 12.6734H15.9234V1.4067C15.9234 1.23431 15.8549 1.06898 15.733 0.947079C15.6111 0.825181 15.4458 0.756699 15.2734 0.756699H0.756714M5.79421 12.6734H3.57338C3.48802 12.6734 3.4035 12.6566 3.32464 12.6239C3.24577 12.5912 3.17412 12.5433 3.11376 12.483C3.0534 12.4226 3.00552 12.351 2.97286 12.2721C2.94019 12.1932 2.92338 12.1087 2.92338 12.0234V6.71503"
+                    stroke="#14A7E0"
+                    stroke-width="1.51338"
+                    stroke-linecap="round"
+                />
+                <path d="M1.84009 4.0067H6.17342" stroke="#14A7E0" stroke-width="1.51338" stroke-linecap="round" stroke-linejoin="round" />
+                <path
+                    d="M15.9233 4.0067H22.0008C22.1265 4.00673 22.2494 4.04317 22.3548 4.11162C22.4601 4.18007 22.5434 4.27758 22.5945 4.39237L24.5337 8.75603C24.5706 8.83889 24.5898 8.92856 24.59 9.01928V12.0234C24.59 12.1087 24.5732 12.1932 24.5405 12.2721C24.5079 12.351 24.46 12.4226 24.3996 12.483C24.3393 12.5433 24.2676 12.5912 24.1888 12.6239C24.1099 12.6566 24.0254 12.6734 23.94 12.6734H21.8817M15.9233 12.6734H17.0067"
+                    stroke="#14A7E0"
+                    stroke-width="1.51338"
+                    stroke-linecap="round"
+                />
+            </svg>
+        `,
+    };
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
         const startTime = Date.now();
@@ -53,6 +81,10 @@
         return o ? s.querySelector(o) : document.querySelector(s);
     }
 
+    function qq(s, o) {
+        return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
     function debounce(func, wait) {
         let timeout;
         return function executedFunction(...args) {
@@ -71,6 +103,7 @@
         } else {
             q("body").classList.remove(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
             window[page_initials] = false;
+            qq(".ab-social-proof-badge").forEach((item) => item.remove());
         }
     }
 
@@ -97,7 +130,11 @@
     }
 
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && document.readyState === "complete");
+        return !!(
+            q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) &&
+            qq(".cart__banner, .summit-Title-root.summary-head.cart__section-title").length === 2 &&
+            document.readyState === "complete"
+        );
     }
 
     async function INIT_PMO32() {
@@ -107,6 +144,20 @@
             await waitForElementAsync(checkForItems);
             q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
             console.table(TEST_CONFIG);
+
+            const TXT = ["Delivered to 8,271 homes this week", "Delivered to 8,200+ homes this week"];
+
+            qq(".cart__banner, .summit-Title-root.summary-head.cart__section-title").forEach((item) => {
+                item.insertAdjacentHTML(
+                    "afterend",
+                    /* HTML */ `
+                        <div class="ab-social-proof-badge">
+                            <div class="ab-social-proof-badge__icon">${ASSETS.truck_svg}</div>
+                            <div class="ab-social-proof-badge__txt">${TXT[test_variation - 1]}</div>
+                        </div>
+                    `,
+                );
+            });
         } catch (error) {
             console.warn(error);
             return false;
