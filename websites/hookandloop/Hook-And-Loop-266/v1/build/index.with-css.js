b0325e71c54958e4052db6ce35df584176176b0c
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-06-24 16:28:29 +0600

    Update: Hook & Loop 266 - H & L - A/B test idea - Show product inventory/lead times on product pages

---
+++ websites/hookandloop/Hook-And-Loop-266/v1/build/index.with-css.js
@@ -0,0 +1,206 @@
+(function () {
+  var interval = setInterval(function () {
+    if (document.head) {
+      // Check if <head> exists
+      clearInterval(interval); // Stop checking once found
+      var style = document.createElement("style");
+      style.innerHTML = `.AB-266 div:has(> .updated-stock-status) p {
+  display: none;
+}
+.AB-266 .ab-stock-status-container {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 2px;
+}
+.AB-266 .ab-stock-status-icon {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.AB-266 .ab-stock-status-icon svg {
+  width: 30px;
+  height: 30px;
+}
+.AB-266 .ab-stock-status-text {
+  font-family: poppins, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 500;
+  color: rgb(55, 65, 81);
+  white-space: nowrap;
+}
+.AB-266 .ab-stock-status-text--in-stock {
+  color: #008800;
+}
+.AB-266 .ab-stock-status-text--low-stock {
+  color: #ff9903;
+}
+`;
+      document.head.appendChild(style);
+      setTimeout(() => {
+        clearInterval(interval); // Clear the interval after 5 seconds
+      }, 5000);
+    }
+  }, 100); // Check every 100ms for <head>
+})();
+/* 
+In Stock 
+Low Stock
+*/
+
+(async () => {
+  const TEST_CONFIG = {
+    client: "Hook & Loop",
+    project: "Hook & Loop",
+    site_url: "https://www.hookandloop.com",
+    test_name:
+      "Hook & Loop 266 - H & L - A/B test idea - Show product inventory/lead times on product pages.",
+    page_initials: "AB-266",
+    test_variation: 1,
+    test_version: 0.0001,
+  };
+
+  const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+  async function waitForElementAsync(
+    predicate,
+    timeout = 20000,
+    frequency = 150,
+  ) {
+    const startTime = Date.now();
+
+    return new Promise((resolve, reject) => {
+      if (typeof predicate === "function" && predicate()) {
+        return resolve(true);
+      }
+
+      const interval = setInterval(() => {
+        const elapsed = Date.now() - startTime;
+
+        if (elapsed >= timeout) {
+          clearInterval(interval);
+          return reject(
+            new Error(
+              `Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`,
+            ),
+          );
+        }
+
+        if (typeof predicate === "function" && predicate()) {
+          clearInterval(interval);
+          return resolve(true);
+        }
+      }, frequency);
+    });
+  }
+
+  function q(s, o) {
+    return o ? s.querySelector(o) : document.querySelector(s);
+  }
+
+  function qq(s, o) {
+    return [...document.querySelectorAll(s)];
+  }
+
+  const DATA = {
+    "in stock": {
+      icon: /* HTML */ `
+        <svg
+          width="640"
+          height="640"
+          viewBox="0 0 640 640"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576ZM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7Z"
+            fill="#008800"
+          />
+        </svg>
+      `,
+      label: "In stock",
+    },
+    "low stock": {
+      icon: /* HTML */ `
+        <svg
+          width="640"
+          height="640"
+          viewBox="0 0 640 640"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576ZM320 384C302.3 384 288 398.3 288 416C288 433.7 302.3 448 320 448C337.7 448 352 433.7 352 416C352 398.3 337.7 384 320 384ZM320 192C301.8 192 287.3 207.5 288.6 225.7L296 329.7C296.9 342.3 307.4 352 319.9 352C332.5 352 342.9 342.3 343.8 329.7L351.2 225.7C352.5 207.5 338.1 192 319.8 192H320Z"
+            fill="#FF9903"
+          />
+        </svg>
+      `,
+      label: "Low stock",
+    },
+  };
+
+  function createLayout(currentStatus) {
+    if (!currentStatus) return;
+
+    console.log(currentStatus);
+
+    qq(".ab-stock-status-container")?.forEach((item) => item.remove());
+
+    const targetNodes = qq("div:has(>.updated-stock-status)");
+    targetNodes.forEach((targetNode) => {
+      targetNode.insertAdjacentHTML(
+        "beforeend",
+        /* HTML */ `
+          <div class="ab-stock-status-container">
+            <div class="ab-stock-status-icon">${DATA[currentStatus].icon}</div>
+            <div
+              class="ab-stock-status-text ab-stock-status-text--${currentStatus
+                .split(" ")
+                .join("-")}"
+            >
+              ${DATA[currentStatus].label}
+            </div>
+          </div>
+        `,
+      );
+    });
+  }
+
+  function customEventListener() {
+    window.addEventListener("configurable-selection-changed", (e) => {
+      const currentStatus =
+        e?.detail?.sstockMessage.toLowerCase().trim() ?? null;
+      createLayout(currentStatus);
+    });
+  }
+
+  function init() {
+    if (window[page_initials] === true) return;
+
+    q("body").classList.add(
+      page_initials,
+      `${page_initials}--v${test_variation}`,
+      `${page_initials}--version:${test_version}`,
+    );
+    window[page_initials] = true;
+    console.table(TEST_CONFIG);
+
+    const currentStatus = q(".updated-stock-status p")
+      .textContent.toLowerCase()
+      .trim();
+    createLayout(currentStatus);
+    customEventListener();
+  }
+
+  function checkForItems() {
+    return !!(
+      q(
+        `body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`,
+      ) && qq("div:has(>.updated-stock-status)")
+    );
+  }
+
+  await waitForElementAsync(checkForItems);
+  init();
+})();
--- websites/hookandloop/Hook-And-Loop-266/v1/index.js
+++ websites/hookandloop/Hook-And-Loop-266/v1/index.js
@@ -55,32 +55,6 @@
         };
     }
 
-    function getCookie(key) {
-        try {
-            if (!key || typeof key !== "string") {
-                // console.error("Invalid key provided to getCookie");
-                return null;
-            }
-
-            // Encode the key to handle special characters
-            const encodedKey = encodeURIComponent(key);
-            const cookies = `; ${document.cookie}`;
-
-            // Find the cookie value
-            const parts = cookies.split(`; ${encodedKey}=`);
-
-            if (parts.length === 2) {
-                const value = parts.pop().split(";").shift();
-                return value ? decodeURIComponent(value) : null;
-            }
-
-            return null;
-        } catch (error) {
-            // console.error(`Error reading cookie "${key}":`, error);
-            return null;
-        }
-    }
-
     function isSafari() {
         const userAgent = navigator.userAgent;
         return /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
@@ -96,14 +70,73 @@
         return new MutationObserver(debouncedUpdate).observe(targetNode, { childList: true, subtree: true, attributes: true });
     }
 
+    const DATA = {
+        "in stock": {
+            icon: /* HTML */ `
+                <svg width="640" height="640" viewBox="0 0 640 640" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576ZM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7Z"
+                        fill="#008800"
+                    />
+                </svg>
+            `,
+            label: "In stock",
+        },
+        "low stock": {
+            icon: /* HTML */ `
+                <svg width="640" height="640" viewBox="0 0 640 640" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576ZM320 384C302.3 384 288 398.3 288 416C288 433.7 302.3 448 320 448C337.7 448 352 433.7 352 416C352 398.3 337.7 384 320 384ZM320 192C301.8 192 287.3 207.5 288.6 225.7L296 329.7C296.9 342.3 307.4 352 319.9 352C332.5 352 342.9 342.3 343.8 329.7L351.2 225.7C352.5 207.5 338.1 192 319.8 192H320Z"
+                        fill="#FF9903"
+                    />
+                </svg>
+            `,
+            label: "Low stock",
+        },
+    };
+
+    function createLayout(currentStatus) {
+        if (!currentStatus) return;
+
+        console.log(currentStatus)
+
+        qq(".ab-stock-status-container")?.forEach((item) => item.remove());
+
+        const targetNodes = qq("div:has(>.updated-stock-status)");
+        targetNodes.forEach((targetNode) => {
+            targetNode.insertAdjacentHTML(
+                "beforeend",
+                /* HTML */ `
+                    <div class="ab-stock-status-container">
+                        <div class="ab-stock-status-icon">${DATA[currentStatus].icon}</div>
+                        <div class="ab-stock-status-text ab-stock-status-text--${currentStatus.split(" ").join("-")}">${DATA[currentStatus].label}</div>
+                    </div>
+                `,
+            );
+        });
+    }
+
+    function customEventListener() {
+        window.addEventListener("configurable-selection-changed", (e) => {
+            const currentStatus = e?.detail?.sstockMessage.toLowerCase().trim() ?? null;
+            createLayout(currentStatus)
+        });
+    }
+
     function init() {
+        if (window[page_initials] === true) return;
+
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        window[page_initials] = true;
         console.table(TEST_CONFIG);
-        // 
+
+        const currentStatus = q(".updated-stock-status p").textContent.toLowerCase().trim();
+        createLayout(currentStatus);
+        customEventListener();
     }
 
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && true);
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && qq("div:has(>.updated-stock-status)"));
     }
 
     await waitForElementAsync(checkForItems);
