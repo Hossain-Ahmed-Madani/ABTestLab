8b38e2d5b1b47deadd5b7ed0a7290975f6f35ecf
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-06-24 15:09:56 +0600

    Init: Hook & Loop 266 - H & L - A/B test idea - Show product inventory/lead times on product pages

---
+++ websites/hookandloop/Hook-And-Loop-266/v1/compiled/index.js
@@ -0,0 +1,53 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "Hook & Loop",
+        project: "Hook & Loop",
+        site_url: "https://www.hookandloop.com",
+        test_name: "Hook & Loop 266 - H & L - A/B test idea - Show product inventory/lead times on product pages.",
+        page_initials: "AB-266",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return o ? s.querySelector(o) : document.querySelector(s);
+    }
+
+    function init() {
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.table(TEST_CONFIG);
+    }
+
+    function checkForItems() {
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && true);
+    }
+
+    await waitForElementAsync(checkForItems);
+    init();
+})();
