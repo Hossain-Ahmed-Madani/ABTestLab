10c15382067461b3eea377021d8c4f42beea0202
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-07-23 15:52:24 +0600

    Init: PLP - Collapse PLP Filters D

---
+++ websites/gurneys/PLP - Collapse PLP Filters D/v1/compiled/index.js
@@ -0,0 +1,62 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "ROI Revolution",
+        project: "Gurneys",
+        site_url: "https://www.gurneys.com",
+        test_name: "PLP - Collapse PLP Filters [D]",
+        page_initials: "AB-COLLAPSE-FILTERS",
+        test_variation: 1 /* 1, 2 */,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return o ? s.querySelector(o) : document.querySelector(s);
+    }
+
+    function qq(s, o) {
+        return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
+    function init() {
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.table(TEST_CONFIG);
+
+        qq(".filter-form__group").forEach((item) => {
+            q(item, ".filter-form__group-toggle").setAttribute("aria-expanded", "false");
+            q(item, ".filter-form__group-filter-wrapper").setAttribute("aria-hidden", "true");
+        });
+    }
+
+    function checkForItems() {
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q(".filter-form__group"));
+    }
+
+    await waitForElementAsync(checkForItems);
+    init();
+})();
