1f61f43911e61e88ec7ca1b91852fe35638aed0a
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-08-05 20:07:19 +0600

    Init: PDP - Fix Overlap on Sticky Search & Sticky ATC [M]

---
+++ websites/steinertractor/PDP - Fix Overlap on Sticky Search and  Sticky ATC M/v1/index.js
@@ -0,0 +1,86 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "ROI Revolution",
+        project: "steinertractor",
+        site_url: "https://www.steinertractor.com",
+        test_name: "PDP - Fix Overlap on Sticky Search & Sticky ATC [M]",
+        page_initials: "AB-FIX-OVERLAP",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return o ? s.querySelector(o) : document.querySelector(s);
+    }
+
+    function qq(s, o) {
+        return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    function init() {
+
+        if(window[page_initials] === true) return;
+        
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        window[page_initials] = true;
+        
+        q("html").insertAdjacentHTML(
+            "beforeend",
+            `
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+        `
+        );
+    }
+
+    function checkForItems() {
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q("#sticky-search-bar"));
+    }
+
+    try {
+        await waitForElementAsync(checkForItems);
+        init();
+    } catch (error) {
+        console.warn(error);
+        return false;
+    }
+})();
