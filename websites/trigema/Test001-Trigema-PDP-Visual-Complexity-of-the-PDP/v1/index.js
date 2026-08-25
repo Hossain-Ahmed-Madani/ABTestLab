ec27a9595b58e7b1fdedc2a232c02af23c5c9dc6
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-08-25 21:57:04 +0600

    Update: Test001 [Trigema] - PDP - Visual Complexity of the PDP"

--- websites/trigema/Test001-Trigema-PDP-Visual-Complexity-of-the-PDP/v1/index.js
+++ websites/trigema/Test001-Trigema-PDP-Visual-Complexity-of-the-PDP/v1/index.js
@@ -35,7 +35,6 @@
         });
     }
 
-
     function q(s, o) {
         return o ? s.querySelector(o) : document.querySelector(s);
     }
@@ -72,12 +71,53 @@
     }
 
     function init() {
+        if (window[page_initials] === true) return;
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        window[page_initials] = true;
+
         console.table(TEST_CONFIG);
+
+        q("head").insertAdjacentHTML(
+            "beforeend",
+            /*  HTML */ `
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+            
+            `,
+        );
+
+        const watchListButton = q(".product-detail-buy-box .product-name__group form.heart");
+        const reviewSection = q(".product-name__group.product-name__group--items-center");
+
+        if (watchListButton) {
+            reviewSection.insertAdjacentElement("beforeend", watchListButton);
+        }
+
+        if (reviewSection) {
+            qq(reviewSection, "p.p--small--medium").forEach((item) => {
+                const txt = item.textContent.trim();
+
+                if (+txt >= 0 && +txt <= 5) {
+                    item.classList.add("ab-review-value");
+                    const newTxt = txt.replace(/\./g, ",");
+                    item.innerText = newTxt + "/5";
+
+                }
+                
+                if (txt.includes("Bewertungen")) {
+                    item.classList.add("ab-review-total");
+                    item.innerText = "(" + txt + ")";
+                }
+            });
+        }
+
+        q('.product-variant__info.product-variant__info--color .product-variant__label').innerText = "Farbe wählen:"
+        q('.product-variant__info.product-variant__info--size .product-variant__label').innerText = "Größe wählen:"
     }
 
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && true);
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q(".product-detail-buy-box .product-name"));
     }
 
     try {
