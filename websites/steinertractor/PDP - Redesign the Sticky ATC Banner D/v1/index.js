b6d191e3ec522de0754d317b0aeba5d9beaa43a7
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-09-07 01:33:47 +0600

    Update: PDP - Redesign the Sticky ATC Banner [D]

--- websites/steinertractor/PDP - Redesign the Sticky ATC Banner D/v1/index.js
+++ websites/steinertractor/PDP - Redesign the Sticky ATC Banner D/v1/index.js
@@ -4,13 +4,12 @@
         project: "steinertractor",
         site_url: "https://www.steinertractor.com/",
         test_name: "PDP - Redesign the Sticky ATC Banner [D]",
-        page_initials: "AB-TEST",
+        page_initials: "AB-STICKY-ATC-BANNER",
         test_variation: 1,
         test_version: 0.0001,
     };
 
     const { page_initials, test_variation, test_version } = TEST_CONFIG;
-
 
     async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
         const startTime = Date.now();
@@ -53,13 +52,49 @@
         return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
     }
 
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    function updateLayout() {
+        console.log(" ======= update layout ========");
+        const productDetailHeader = q(".product-detail-header-section:not(:has(.container))");
+        if (!productDetailHeader) return;
+
+        const container = document.createElement('div');
+        container.classList.add('container')
+        qq(productDetailHeader, ":scope > div").forEach(item => container.appendChild(item));
+        productDetailHeader.appendChild(container)
+
+        q(".container#shop").insertAdjacentElement("beforebegin", productDetailHeader);
+        q(".header-section-1").insertAdjacentElement("beforeend", q(".header-section-2 h4.title"));
+        q('.header-section-4 .cart.clearfix').insertAdjacentElement("afterbegin", q('.header-section-3 h4'));
+    }
+
+    function mutationObserverFunction() {
+        const targetNode = q(".container#shop");
+        const debouncedUpdate = debounce(updateLayout, 100);
+        return new MutationObserver(updateLayout).observe(targetNode, { childList: true, subtree: false, attributes: false });
+    }
+
     function init() {
         q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
         console.table(TEST_CONFIG);
+
+        updateLayout();
+        mutationObserverFunction();
     }
 
     function checkForItems() {
-        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && true);
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q(".product-detail-header-section") && q(".container#shop"));
     }
 
     try {
