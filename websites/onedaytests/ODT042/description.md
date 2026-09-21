842bc8ff480fbebe22681cf41fd9bf50a47d4102
Hossain-Ahmed-Madani <hossain@echologyx.com>
2026-09-21 14:52:53 +0600

    Init: ODT042

---
+++ websites/onedaytests/ODT042/description.md
@@ -0,0 +1,35 @@
+Type: AB Test
+
+Objective: IF we reposition the interactive assessment further up the page into the primary viewport,
+
+THEN we will increase quiz interaction, Add-to-Cart rates, and overall Revenue Per Visitor (RPV),
+
+BECAUSE surfacing highest-engaging asset earlier in the user journey eliminates scroll friction and allows visitors to immediately validate their symptoms, guiding them directly to right test.
+
+url: https://onedaytests.com/pages/testosterone-replacement-therapy-subscription
+audience: all
+KPI: Quiz initiation (number of clicks on the first step), number of finished quiz, visits on the testosterone PDP, add to cart on testosterone page, app revenue
+
+design: https://www.figma.com/design/mnZNCGlHKh0hu37n9rAomj/OTD?node-id=3076-24&p=f&t=hLhCsVi6wwuhGotl-0
+
+we want to have the quiz right above the hero section
+
+Goals:
+
+URL Targeting:
+
+Triggers:
+
+Audience Targeting:
+
+Devices:
+
+Traffic Split:
+
+Link to main ticket:
+
+Additional notes to consider:
+
+Code reviewed by:
+
+Peer reviewed by:
---
+++ websites/onedaytests/ODT042/v1/index.js
@@ -0,0 +1,82 @@
+(async () => {
+    const TEST_CONFIG = {
+        client: "Converted",
+        project: "One Day Test",
+        site_url: "https://onedaytests.com",
+        test_name: "ODT042 - Above the fold quiz placement (TRT)",
+        page_initials: "AB-ODT042",
+        test_variation: 1,
+        test_version: 0.0001,
+    };
+
+    const { page_initials, test_variation, test_version } = TEST_CONFIG;
+
+    async function waitForElementAsync(predicate, timeout = 20000, frequency = 150) {
+        const startTime = Date.now();
+
+        return new Promise((resolve, reject) => {
+            if (typeof predicate === "function" && predicate()) {
+                return resolve(true);
+            }
+
+            const interval = setInterval(() => {
+                const elapsed = Date.now() - startTime;
+
+                if (elapsed >= timeout) {
+                    clearInterval(interval);
+                    return reject(new Error(`Timeout of ${timeout}ms reached while waiting for condition: ${predicate.toString()}`));
+                }
+
+                if (typeof predicate === "function" && predicate()) {
+                    clearInterval(interval);
+                    return resolve(true);
+                }
+            }, frequency);
+        });
+    }
+
+    function q(s, o) {
+        return o ? s.querySelector(o) : document.querySelector(s);
+    }
+
+    function qq(s, o) {
+        return o ? [...s.querySelectorAll(o)] : [...document.querySelectorAll(s)];
+    }
+
+    function debounce(func, wait) {
+        let timeout;
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    function isSafari() {
+        const userAgent = navigator.userAgent;
+        return /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
+    }
+
+    function isTouchEnabled() {
+        return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    }
+
+    function init() {
+        q("body").classList.add(page_initials, `${page_initials}--v${test_variation}`, `${page_initials}--version:${test_version}`);
+        console.log(TEST_CONFIG);
+    }
+
+    function checkForItems() {
+        return !!(q(`body:not(.${page_initials}):not(.${page_initials}--v${test_variation})`) && q('div[class*="ai-subscription-tiers__grid"] > div[class*="ai-subscription-tier"]'));
+    }
+
+    try {
+        await waitForElementAsync(checkForItems);
+        init();
+    } catch (error) {
+        return false;
+    }
+})();
